9c07244f1a1cb46f9cfea9c11ccba0f1ffbea7ab
Dalton Pettus <daltpettus@gmail.com>
2025-03-19 14:37:59 -0700

    got sudoku solver working, still need refactor

--- CodeWars/sudokuSolver.js
+++ CodeWars/sudokuSolver.js
@@ -1,16 +1,26 @@
-function getNumsMissingMaps() {
-    const numsRequired = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+function getNumsMaps() {
+    // const numsRequired = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
-    const sectionNumsMissing = new Map();
-    const rowNumsMissing = new Map();
-    const colNumsMissing = new Map();
+    // const sectionNumsMissingMap = new Map();
+    // const rowNumsMissingMap = new Map();
+    // const colNumsMissingMap = new Map();
+    // for (let i = 0; i < 9; i++) {
+    //     sectionNumsMissingMap.set(i, new Set(numsRequired));
+    //     rowNumsMissingMap.set(i, new Set(numsRequired));
+    //     colNumsMissingMap.set(i, new Set(numsRequired));
+    // }
+
+    // return { sectionNumsMissingMap, rowNumsMissingMap, colNumsMissingMap };
+    const sectionNumsMap = new Map();
+    const rowNumsMap = new Map();
+    const colNumsMap = new Map();
     for (let i = 0; i < 9; i++) {
-        sectionNumsMissing.set(i, new Set(numsRequired));
-        rowNumsMissing.set(i, new Set(numsRequired));
-        colNumsMissing.set(i, new Set(numsRequired));
+        sectionNumsMap.set(i, new Set());
+        rowNumsMap.set(i, new Set());
+        colNumsMap.set(i, new Set());
     }
 
-    return { sectionNumsMissing, rowNumsMissing, colNumsMissing };
+    return { sectionNumsMap, rowNumsMap, colNumsMap };
 }
 
 function getSection(row, col) {
@@ -29,36 +39,102 @@ function getSection(row, col) {
     }
 }
 
-
 function sudoku(puzzle) {
+    const getMissingNum = (numSet) => {
+        for (let num = 1; num < 10; num++) {
+            if (!numSet.has(num)) return num;
+        }
+    }
+
     const n = puzzle.length;
-    const { sectionNumsMissing, rowNumsMissing, colNumsMissing } = getNumsMissingMaps();
-    const emptyCellPositions = [];
-    
+    let remainingCells = n * n;
+    const { sectionNumsMap, rowNumsMap, colNumsMap } = getNumsMaps();
+    let emptyCells = new Set();
+
     for (let row = 0; row < n; row++) {
         for (let col = 0; col < n; col++) {
-            const num = puzzle[row][col];
-            const section = getSection(row, col);
-
-            if (num === 0) {
-                emptyCellPositions.push({ section, row, col });
+            let section = getSection(row, col);
+            let num = puzzle[row][col];
+            if (num !== 0) {
+                remainingCells--;
+                sectionNumsMap.get(section).add(num);
+                rowNumsMap.get(row).add(num);
+                colNumsMap.get(col).add(num);
             } else {
-                sectionNumsMissing.get(section).delete(num);
-                rowNumsMissing.get(row).delete(num);
-                colNumsMissing.get(col).delete(num);
+                emptyCells.add({ section, row, col, possibleNums: new Set([1, 2, 3, 4, 5, 6, 7, 8, 9])})
             }
         }
     }
 
+    let toDelete = [];
+    while (remainingCells !== 0) {
+        for (let emptyCell of emptyCells) {
+            const { section, row, col, possibleNums } = emptyCell;
+            const sectionNums = sectionNumsMap.get(section);
+            const rowNums = rowNumsMap.get(row);
+            const colNums = colNumsMap.get(col);
     
+            for (let num = 1; num < 10; num++) {
+                if (sectionNums.has(num) || rowNums.has(num) || colNums.has(num)) {
+                    possibleNums.delete(num);
+                }
+            }
+    
+            if (possibleNums.size === 1) {
+                remainingCells--;
+                const [num] = possibleNums;
+                console.log(row, col, num);
+                puzzle[row][col] = num;
+                sectionNums.add(num);
+                rowNums.add(num);
+                colNums.add(num);
+                toDelete.push(emptyCell);
+                continue;
+            }
+    
+            if (sectionNums.size === 8) {
+                remainingCells--;
+                const num = getMissingNum(sectionNums);
+                console.log(row, col, num);
+                sectionNums.add(num);
+                rowNums.add(num);
+                colNums.add(num);
+                toDelete.push(emptyCell);
+                continue;
+            }
+            if (rowNums.size === 8) {
+                remainingCells--;
+                const num = getMissingNum(rowNums);
+                console.log(row, col, num);
+                sectionNums.add(num);
+                rowNums.add(num);
+                colNums.add(num);
+                toDelete.push(emptyCell);
+                continue;
+            }
+            if (colNums.size === 8) {
+                remainingCells--;
+                const num = getMissingNum(colNums);
+                console.log(row, col, num);
+                sectionNums.add(num);
+                rowNums.add(num);
+                colNums.add(num);
+                toDelete.push(emptyCell);
+                continue;
+            }
+        }
+    
+        for (let emptyCell of toDelete) emptyCells.delete(emptyCell);
+        toDelete = [];
+    }
 
-    return emptyCellPositions;
+    return puzzle;
 }
 
 const puzzle = [
     [5,3,0,0,7,0,0,0,0],
     [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0, 0,6,0],
+    [0,9,8,0,0,0,0,6,0],
     [8,0,0,0,6,0,0,0,3],
     [4,0,0,8,0,3,0,0,1],
     [7,0,0,0,2,0,0,0,6],
@@ -66,10 +142,33 @@ const puzzle = [
     [0,0,0,4,1,9,0,0,5],
     [0,0,0,0,8,0,0,7,9]];
 
-console.log(sudoku(puzzle));
+//printPuzzle(puzzle);
+
+sudoku(puzzle);
 //console.log(getSection(7, 0));
+printPuzzle(puzzle);
 
 
+
+function printPuzzle(puzzle) {
+    for (let row = 0; row < puzzle.length; row++) {
+        let rowStr = '';
+        for (let col = 0; col < 9; col++) {
+            rowStr += puzzle[row][col] + '|'
+            if (col === 2 || col === 5) {
+                rowStr = rowStr.slice(0, rowStr.length - 1) + '#';
+            }
+        }
+
+        console.log(rowStr);
+        if (row === 2 || row === 5) {
+            console.log('###################')
+        } else {
+            console.log('-----#-----#-------');
+        }
+    }
+    console.log('\n');
+}
 
 // function getSection(row, col) {
 //     //consider only having to build sections object array once
