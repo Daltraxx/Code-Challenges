15679306d62623bd9f782145501aae4098dbcc87
Dalton Pettus <daltpettus@gmail.com>
2025-03-24 10:36:57 -0700

    circle slash mathematical solution

--- CodeWars/circleSlash.js
+++ CodeWars/circleSlash.js
@@ -1,31 +1,40 @@
-function circleSlash(n) {
-    let people = [];
-    for (let person = 1; person <= n; person += 2) {
-        people.push(person);
-    }
+// function circleSlash(n) {
+//     let people = [];
+//     for (let person = 1; person <= n; person += 2) {
+//         people.push(person);
+//     }
 
-    //if people in round not even, last person is front of next round
-    if (n % 2 !== 0) {
-        people.unshift(people.pop());
-    }
+//     //if people in round not even, last person is front of next round
+//     if (n % 2 !== 0) {
+//         people.unshift(people.pop());
+//     }
     
-    while (people.length > 1) {
-        const remainingPeople = [];
-        for (let i = 0; i < people.length; i += 2) {
-            remainingPeople.push(people[i]);
-        }
+//     while (people.length > 1) {
+//         const remainingPeople = [];
+//         for (let i = 0; i < people.length; i += 2) {
+//             remainingPeople.push(people[i]);
+//         }
 
-        //if people in round not even, last person is front of next round
-        if (people.length % 2 !== 0) {
-            remainingPeople.unshift(remainingPeople.pop());
-        }
+//         //if people in round not even, last person is front of next round
+//         if (people.length % 2 !== 0) {
+//             remainingPeople.unshift(remainingPeople.pop());
+//         }
 
-        people = remainingPeople;
+//         people = remainingPeople;
+//     }
+
+//     return people[0];
+// } O(n log n) solution
+
+//mathematical solution
+function circleSlash(n) {
+    let highestPowerOfTwo = 1;
+    while (highestPowerOfTwo * 2 <= n) {
+        highestPowerOfTwo *= 2;
     }
 
-    return people[0];
+    return (n - highestPowerOfTwo) * 2 + 1;
 }
-
 
 const n = 31;
 console.log(circleSlash(n));
