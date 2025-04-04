ca52d8f5e6cd2b0fd71723a73816511f2194dbda
Dalton Pettus <daltpettus@gmail.com>
2025-04-04 14:12:21 -0700

    pagination helper class challenge

---
+++ CodeWars/paginationHelper.js
@@ -0,0 +1,44 @@
+class PaginationHelper {
+    constructor(collection, itemsPerPage) {
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
+        this.pageIndices = new Map();
+    }
+
+    setPageIndices() {
+        for (let i = 0; i < this.itemCount(); i++) {
+            this.pageIndices.set(i, Math.floor(i / this.itemsPerPage));
+        }
+    }
+
+    itemCount() {
+        return this.collection.length;
+    }
+
+    pageCount() {
+        return Math.ceil(this.itemCount() / this.itemsPerPage);
+    }
+
+    pageItemCount(pageIndex) {
+        if (pageIndex > this.pageCount() - 1 || pageIndex < 0) {
+            return -1;
+        } else if (pageIndex < this.pageCount() - 1 || this.itemCount() % this.itemsPerPage === 0) {
+            return this.itemsPerPage;
+        } else {
+            return this.itemCount() % this.itemsPerPage;
+        } 
+    }
+
+    pageIndex(itemIndex) {
+        if (itemIndex < 0 || itemIndex > this.itemCount() - 1) return -1;
+
+        return Math.floor(itemIndex / this.itemsPerPage);
+    }
+}
+
+const helper = new PaginationHelper(new Array(54), 9);
+console.log(helper.itemCount());
+console.log(helper.pageCount());
+console.log(helper.pageItemCount(-1));
+// console.log(helper.pageIndices);
+console.log(helper.pageIndex(5));
