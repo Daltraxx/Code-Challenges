f150c7535c7b080307dad09b23e0b9bb8abbeb67
Dalton Pettus <daltpettus@gmail.com>
2025-04-11 21:04:23 -0700

    the poet and the pendulum challenge complete

--- CodeWars/thePoetAndThePendulum.js
+++ CodeWars/thePoetAndThePendulum.js
@@ -1,12 +1,28 @@
 function pendulum(values) {
     const sortedValues = values.toSorted((a, b) => a - b);
     const n = values.length;
-    const result = new Array(n);
-    let middle = n % 2 === 0 ? Math.floor((n - 1) / 2) : Math.floor(n / 2);
-    
+    const pendulumArr = new Array(n);
+    const middle = n % 2 === 0 ? Math.floor((n - 1) / 2) : Math.floor(n / 2);
 
-    return sortedValues;
+    pendulumArr[middle] = sortedValues[0];
+
+    let middleRight = middle + 1;
+    let middleLeft = middle - 1;
+    let directionRight = true;
+
+    for (let i = 1; i < n; i++) {
+        if (directionRight) {
+            pendulumArr[middleRight] = sortedValues[i];
+            middleRight++;
+        } else {
+            pendulumArr[middleLeft] = sortedValues[i];
+            middleLeft--;
+        }
+        directionRight = !directionRight;
+    }
+
+    return pendulumArr;
 }
 
-const values = [10, 6, 5, 6, 8, 1, 5, 4];
+const values = [11, -16, -18, 13, -11, -12, 3, 18];
 console.log(pendulum(values));
