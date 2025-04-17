054cc514ab59ff3df93b716f49eccb5d44733da9
Dalton Pettus <daltpettus@gmail.com>
2025-04-17 10:54:42 -0700

    last stone weight heap solution

---
+++ Heaps/LastStoneWeight.java
@@ -0,0 +1,19 @@
+package Heaps;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int stone : stones) heap.add(stone);
+        
+        while (heap.size() > 1) {
+            int remainingStone = heap.remove() - heap.remove();
+            if (remainingStone > 0) heap.add(remainingStone);
+        }
+
+        return heap.isEmpty() ? 0 : heap.peek();
+    }
+}
