1a3007d9e035a6d5588c0eefa9d8b033a24ec0f1
Dalton Pettus <daltpettus@gmail.com>
2025-04-27 22:01:18 -0700

    kth largest element min heap solution

---
+++ Heaps/KthLargestElement.java
@@ -0,0 +1,15 @@
+package Heaps;
+
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) minHeap.remove();
+        }
+        
+        return minHeap.peek();
+    }
+}
