4588e111a6fb31bedb42bdb6c934a54c138fa568
Dalton Pettus <daltpettus@gmail.com>
2025-05-03 20:46:50 -0700

    add whiteboard file

---
+++ Whiteboard.java
@@ -0,0 +1,8 @@
+public class Whiteboard {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+
+        char charAtIndex4 = str.charAt(4);
+        System.out.println("Character at index 5: " + charAtIndex4);
+    }
+}
