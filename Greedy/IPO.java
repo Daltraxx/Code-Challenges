8533c0d1e77ca2c5c7a1f581819131c6a657c469
Dalton Pettus <daltpettus@gmail.com>
2025-05-14 20:56:16 -0700

    start ipo code challenge

--- Greedy/IPO.java
+++ Greedy/IPO.java
@@ -1,5 +1,9 @@
+import java.util.PriorityQueue;
+
 public class IPO {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> { 
+            return profits[a] - profits[b];
+        });
     }
 }
