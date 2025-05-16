b597a22eea2527d6c0aa8d303b9c0200d3e92927
Dalton Pettus <daltpettus@gmail.com>
2025-05-15 21:25:03 -0700

    first solution IPO

--- Greedy/IPO.java
+++ Greedy/IPO.java
@@ -1,9 +1,32 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class IPO {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> { 
-            return profits[a] - profits[b];
-        });
+        int n = profits.length;
+        int[][] projects = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            projects[i] = new int[]{capital[i], profits[i]};
+        }
+        
+        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        int i = 0;
+        
+        for (int j = 0; j < k; j++) {
+            while (i < n && projects[i][0] <= w) {
+                heap.add(projects[i][1]);
+                i++;
+            }
+            
+            if (heap.isEmpty()) {
+                return w;
+            }
+            
+            w += heap.remove();
+        }
+        
+        return w;
     }
 }
