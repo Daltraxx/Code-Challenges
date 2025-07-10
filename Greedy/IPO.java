4ccdf717b73d4b648b0258f928a022590370c2c8
Dalton Pettus <daltpettus@gmail.com>
2025-07-09 20:48:35 -0700

    IPO max heap solution

--- Greedy/IPO.java
+++ Greedy/IPO.java
@@ -9,26 +9,28 @@ public class IPO {
         for (int i = 0; i < n; i++) {
             projects[i] = new int[]{capital[i], profits[i]};
         }
-        
+
         Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        int i = 0;
         
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int i = 0;
         for (int j = 0; j < k; j++) {
             while (i < n && projects[i][0] <= w) {
                 maxHeap.add(projects[i][1]);
                 i++;
             }
-            
-            if (maxHeap.isEmpty()) {
-                return w;
-            }
-            
+
+            if (maxHeap.isEmpty()) return w;
+
             w += maxHeap.remove();
         }
-        
+
         return w;
     }
 }
 
-// problem for tomorrow
+// This algorithm has a time complexity of O((k+n)⋅logn), where n is the number of projects given. 
+// The heap's max size is n, which means its operations are logn in the worst case, and we do k + n operations (k pop operations, n push operations). 
+// The sort at the start also costs O(n⋅logn), but this doesn't change the complexity. 
+// The space complexity is O(n) due to the heap.
