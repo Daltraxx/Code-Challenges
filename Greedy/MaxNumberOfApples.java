af278b07d10d713c560d1630078619cd7a6865d3
Dalton Pettus <daltpettus@gmail.com>
2025-09-19 11:55:54 -0700

    sort array without mutating original array

--- Greedy/MaxNumberOfApples.java
+++ Greedy/MaxNumberOfApples.java
@@ -3,11 +3,12 @@ import java.util.Arrays;
 public class MaxNumberOfApples {
   public int maxNumberOfApples(int[] weight) {
     int MAX_WEIGHT = 5000;
-    Arrays.sort(weight);
+    int[] sortedWeights = weight.clone();
+    Arrays.sort(sortedWeights);
     int appleCount = 0;
     int currentWeight = 0;
-    for (int i = 0; i < weight.length; i++) {
-      int appleWeight = weight[i];
+    for (int i = 0; i < sortedWeights.length; i++) {
+      int appleWeight = sortedWeights[i];
       if (currentWeight + appleWeight > MAX_WEIGHT)
         return appleCount;
       
