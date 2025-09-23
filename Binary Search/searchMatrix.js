359f55f5ab6c690b4eefd579ca6fce6f73feb31b
Dalton Pettus <daltpettus@gmail.com>
2025-09-23 08:18:51 -0700

    fix

--- Binary Search/searchMatrix.js
+++ Binary Search/searchMatrix.js
@@ -17,7 +17,7 @@ const searchMatrix = (matrix, target) => {
   let row;
   if (matrix[mid][m - 1] >= target) {
     row = matrix[mid];
-  } else if (matrix[mid][m - 1] < n && matrix[mid][m - 1] < target) {
+  } else if (mid < n && matrix[mid][m - 1] < target) {
     row = matrix[mid + 1];
   } else {
     return false;
@@ -38,7 +38,11 @@ const searchMatrix = (matrix, target) => {
   return false;
 };
 
-const matrix = [[1]],
-  target = 2;
+const matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 50],
+  ],
+  target = 10;
 
 console.log(searchMatrix(matrix, target));
