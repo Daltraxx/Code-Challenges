56abae8e680eeda18a5180e5249d33f38283169b
Dalton Pettus <daltpettus@gmail.com>
2025-09-24 09:59:56 -0700

    improve 2d matrix binary search solution

--- Binary Search/searchMatrix.js
+++ Binary Search/searchMatrix.js
@@ -1,34 +1,13 @@
 const searchMatrix = (matrix, target) => {
   const n = matrix.length, m = matrix[0].length;
-  let top = 0,
-    bottom = n - 1;
-  let mid;
-  while (top <= bottom) {
-    mid = Math.floor((top + bottom) / 2);
-    if (matrix[mid][m - 1] === target) return true;
-    
-    if (matrix[mid][m - 1] > target) {
-      bottom = mid - 1;
-    } else {
-      top = mid + 1;
-    }
-  }
-
-  let row;
-  if (matrix[mid][m - 1] >= target) {
-    row = matrix[mid];
-  } else if (mid < n - 1 && matrix[mid][m - 1] < target) {
-    row = matrix[mid + 1];
-  } else {
-    return false;
-  }
-
-  let left = 0, right = m - 1;
+  let left = 0, right = m * n - 1;
   while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-    if (row[mid] === target) return true;
-
-    if (row[mid] > target) {
+    const mid = Math.floor((left + right) / 2);
+    const row = Math.floor(mid / 2);
+    const col = mid % m;
+    const midElement = matrix[row][col];
+    if (midElement === target) return true;
+    if (midElement > target) {
       right = mid - 1;
     } else {
       left = mid + 1;
@@ -38,7 +17,11 @@ const searchMatrix = (matrix, target) => {
   return false;
 };
 
-const matrix = [[1]],
-  target = 2;
+const matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60],
+  ],
+  target = 3;
 
 console.log(searchMatrix(matrix, target));
