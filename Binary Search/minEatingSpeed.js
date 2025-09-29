475bcefe81ef32e6974e3db3833ff5be4934402f
Dalton Pettus <daltpettus@gmail.com>
2025-09-29 12:51:23 -0700

    koko eating bananas binary search solution space solution

---
+++ Binary Search/minEatingSpeed.js
@@ -0,0 +1,22 @@
+const minEatingSpeed = (piles, h) => {
+  const check = k => {
+    let hours = 0;
+    for (let bananas of piles) {
+      hours += Math.ceil(bananas / k);
+    }
+
+    return hours <= h;
+  }
+
+  let left = 1, right = Math.max(...piles);
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (check(mid)) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+}
