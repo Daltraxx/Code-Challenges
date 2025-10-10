6a415c6320a954f640b15d727f3a30e74277abb8
Dalton Pettus <dpettus0713@gmail.com>
2025-10-09 18:38:41 -0700

    formatting

--- Backtracking/permute.js
+++ Backtracking/permute.js
@@ -1,22 +1,22 @@
 const permute = (nums) => {
-    const backtrack = (current) => {
-        if (current.length === nums.length) {
-            permutations.push(current);
-            return;
-        }
-        for (let num of nums) {
-            if (!current.includes(num)) {
-                current.push(num)
-                backtrack(current);
-                current.pop();
-            }
-        }
+  const backtrack = (current) => {
+    if (current.length === nums.length) {
+      permutations.push(current);
+      return;
     }
+    for (let num of nums) {
+      if (!current.includes(num)) {
+        current.push(num);
+        backtrack(current);
+        current.pop();
+      }
+    }
+  };
 
-    const permutations = [];
-    backtrack(([]));
-    return permutations;
-}
+  const permutations = [];
+  backtrack([]);
+  return permutations;
+};
 
-const nums = [1,2,3];
+const nums = [1, 2, 3];
 console.log(permute(nums));
