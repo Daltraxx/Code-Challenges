b15f3c370f157a928b58a99940af345d0dfd384f
Dalton Pettus <daltpettus@gmail.com>
2025-10-11 07:02:18 -0700

    remove unnecessary set and includes step

--- Backtracking/subsets.js
+++ Backtracking/subsets.js
@@ -1,18 +1,12 @@
 const subsets = (nums) => {
   const subsetsArr = [];
-  const used = new Set();
 
   const backtrack = (curr, i) => {
     subsetsArr.push([...curr]);
     for (i; i < nums.length; i++) {
-      const num = nums[i];
-      if (!used.has(num)) {
-        used.add(num);
-        curr.push(num);
-        backtrack(curr, i + 1);
-        used.delete(num);
-        curr.pop();
-      }
+      curr.push(nums[i]);
+      backtrack(curr, i + 1);
+      curr.pop();
     }
   };
 
