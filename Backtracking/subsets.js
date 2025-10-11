1c25db1dfbdad2a41086a1369ae55a434893b4b1
Dalton Pettus <daltpettus@gmail.com>
2025-10-11 06:37:23 -0700

    subsets backtracking solution

---
+++ Backtracking/subsets.js
@@ -0,0 +1,19 @@
+const subsets = (nums) => {
+  const subsetsArr = [];
+  const backtrack = (curr, i) => {
+    subsetsArr.push(curr);
+    for (i; i < nums.length; i++) {
+      const num = nums[i];
+      if (!curr.includes(num)) {
+        curr.push(num);
+        backtrack(curr, i + 1);
+        curr.pop();
+      }
+    }
+  }
+  backtrack([], 0);
+  return subsetsArr;
+}
+
+const nums = [1, 2, 3];
+console.log(subsets(nums));
