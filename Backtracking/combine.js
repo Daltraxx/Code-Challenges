23be363b08230351261745fe48c9c4fd136728d2
Dalton Pettus <dpettus0713@gmail.com>
2025-10-12 13:38:12 -0700

    test

--- Backtracking/combine.js
+++ Backtracking/combine.js
@@ -2,7 +2,7 @@ const combine = (n, k) => {
   const combinations = [];
 
   const backtrack = (curr, i) => {
-    if ((curr.length = k)) {
+    if (curr.length === k) {
       combinations.push([...curr]);
       return;
     }
@@ -17,3 +17,6 @@ const combine = (n, k) => {
   backtrack([], 1);
   return combinations;
 };
+
+const n = 4, k = 2;
+console.log(combine(n, k));
