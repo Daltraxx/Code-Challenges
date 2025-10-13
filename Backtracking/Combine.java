fce3151bd5155ea798905616408f2d5ff5d4e24f
Dalton Pettus <dpettus0713@gmail.com>
2025-10-12 18:45:43 -0700

    finish java backtracking solution

--- Backtracking/Combine.java
+++ Backtracking/Combine.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Combine {
     List<List<Integer>> combinations;
+    int n;
+    int k;
     public List<List<Integer>> combine(int n, int k) {
         combinations = new ArrayList<>();
+        this.n = n;
+        this.k = k;
+        backtrack(new ArrayList<>(), 1);
+        return combinations;
+    }
+
+    private void backtrack(List<Integer> curr, int i) {
+        if (curr.size() == n) {
+            combinations.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int j = i; j <= n; j++) {
+            curr.add(j);
+            backtrack(curr, j + 1);
+            curr.remove(curr.size() - 1);
+        }
     }
 }
