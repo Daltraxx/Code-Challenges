fd5ae4abcf69efe658f87788686db5d45c306f09
Dalton Pettus <daltpettus@gmail.com>
2025-10-13 07:34:37 -0700

    apply fix, must pop before returning

--- Backtracking/allPathsSourceTarget.js
+++ Backtracking/allPathsSourceTarget.js
@@ -3,20 +3,26 @@ const allPathsSourceTarget = (graph) => {
   const n = graph.length;
 
   const backtrack = (curr, i) => {
+    curr.push(i);
+
     if (i === n - 1) {
       paths.push([...curr]);
+      curr.pop(i);
       return;
     }
 
     const neighbors = graph[i];
 
     for (let neighbor of neighbors) {
-      curr.push(neighbor);
       backtrack(curr, neighbor);
-      curr.pop();
     }
+
+    curr.pop(i);
   }
 
   backtrack([], 0);
   return paths;
 }
+
+const graph = [[1, 2], [3], [3], []];
+console.log(allPathsSourceTarget(graph));
