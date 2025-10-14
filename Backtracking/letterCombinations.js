7ae81190f13cf9419ab58db46bbbb7bbb45bf77c
Dalton Pettus <daltpettus@gmail.com>
2025-10-14 07:26:51 -0700

    Letter combinations of a phone number backtracking solution

---
+++ Backtracking/letterCombinations.js
@@ -0,0 +1,33 @@
+const letterCombinations = (digits) => {
+  const n = digits.length;
+  const numToLetterMap = new Map([
+    [1, []],
+    [2, ['a', 'b', 'c']],
+    [3, ['d', 'e', 'f']],
+    [4, ['g', 'h', 'i']],
+    [5, ['j', 'k', 'l']],
+    [6, ['m', 'n', 'o']],
+    [7, ['p', 'q', 'r', 's']],
+    [8, ['t', 'u', 'v']],
+    [9, ['w', 'x', 'y', 'z']]
+  ]);
+
+  const combinations = [];
+
+  const backtrack = (curr, i) => {
+    if (curr === n) {
+      combinations.push(curr);
+      return;
+    }
+
+    const letters = numToLetterMap.get(curr[i]);
+    for (let letter of letters) {
+      curr += letter;
+      backtrack(curr, i + 1);
+      curr = curr.slice(0, -1);
+    }
+  }
+
+  backtrack('', 0);
+  return combinations;
+}
