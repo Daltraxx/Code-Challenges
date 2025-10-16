d89ccad2abba364f32fbdc7ff6b60e22d277d063
Dalton Pettus <daltpettus@gmail.com>
2025-10-16 07:49:59 -0700

    gix bugs

--- Backtracking/totalNQueens.js
+++ Backtracking/totalNQueens.js
@@ -1,12 +1,10 @@
 const totalNQueens = (n) => {
-  const attackedRows = new Set();
   const attackedCols = new Set();
   const attackedDiagonals = new Set();
   const attackedAntiDiagonals = new Set();
 
   const isSafe = (row, col) => {
     return (
-      !attackedRows.has(row) &&
       !attackedCols.has(col) &&
       !attackedDiagonals.has(row - col) &&
       !attackedAntiDiagonals.has(row + col)
@@ -14,14 +12,12 @@ const totalNQueens = (n) => {
   };
 
   const setPiece = (row, col) => {
-    attackedRows.add(row);
     attackedCols.add(col);
     attackedDiagonals.add(row - col);
     attackedAntiDiagonals.add(row + col);
   };
 
   const removePiece = (row, col) => {
-    attackedRows.delete(row);
     attackedCols.delete(col);
     attackedDiagonals.delete(row - col);
     attackedAntiDiagonals.delete(row + col);
@@ -29,24 +25,22 @@ const totalNQueens = (n) => {
 
   let solutionCount = 0;
 
-  const backtrack = (row, col) => {
-    if ((row = n)) {
+  const backtrack = (row) => {
+    if (row === n) {
       solutionCount++;
       return;
     }
 
-    for (row; row < n; row++) {
-      for (col; col < n; col++) {
-        if (isSafe(row, col)) {
-          setPiece(row, col);
-          backtrack(row, col + 1);
-          removePiece(row, col);
-        }
+    for (let col = 0; col < n; col++) {
+      if (isSafe(row, col)) {
+        setPiece(row, col);
+        backtrack(row + 1);
+        removePiece(row, col);
       }
     }
   };
 
-  backtrack(0, 0);
+  backtrack(0);
   return solutionCount;
 };
 
