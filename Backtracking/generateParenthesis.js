0e863e08f55d2b089cf40fc53060a31990a7529d
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 09:36:27 -0700

    refactor

--- Backtracking/generateParenthesis.js
+++ Backtracking/generateParenthesis.js
@@ -1,22 +1,22 @@
 const generateParenthesis = (n) => {
   const combinations = [];
   const comboLength = 2 * n;
+
   const backtrack = (curr, leftCount, rightCount) => {
-    if (curr.length === comboLength && leftCount === rightCount) {
+    if (curr.length === comboLength) {
       combinations.push(curr);
       return;
     }
 
-    if (curr.length < comboLength) {
-      if (leftCount < n) backtrack(curr + "(", leftCount + 1, rightCount);
-      if (leftCount > rightCount) backtrack(curr + ')', leftCount, rightCount + 1);
-    }
-    
-  }
+    if (leftCount < n)
+      backtrack(curr + "(", leftCount + 1, rightCount);
+    if (leftCount > rightCount)
+      backtrack(curr + ")", leftCount, rightCount + 1);
+  };
 
-  backtrack('(', 1, 0);
+  backtrack("(", 1, 0);
   return combinations;
-}
+};
 
 const n = 1;
 console.log(generateParenthesis(n));
