c3ba8473f56d5771c131597fc4cebd365ba175ec
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 09:07:56 -0700

    Generate Parenthesis backtracking first attempt

---
+++ Backtracking/generateParenthesis.js
@@ -0,0 +1,19 @@
+const generateParenthesis = (n) => {
+  const combinations = [];
+  const comboLength = 2 * n;
+  const backtrack = (curr, leftCount) => {
+    if (curr.length === comboLength && leftCount === 0) {
+      combinations.push(curr);
+      return;
+    }
+
+    if (curr.length < comboLength) {
+      backtrack(curr + '(', leftCount + 1);
+      if (leftCount - 1 >= 0) backtrack(curr + ')', leftCount - 1);
+    }
+    
+  }
+
+  backtrack('(', 1);
+  return combinations;
+}
