4a65385a6a3cb148c6046f231a66489612028838
Dalton Pettus <daltpettus@gmail.com>
2025-10-18 09:30:25 -0700

    modify

--- Backtracking/generateParenthesis.js
+++ Backtracking/generateParenthesis.js
@@ -1,20 +1,20 @@
 const generateParenthesis = (n) => {
   const combinations = [];
   const comboLength = 2 * n;
-  const backtrack = (curr, leftCount) => {
+  const backtrack = (curr, leftCount, rightCount) => {
     if (curr.length === comboLength && leftCount === 0) {
       combinations.push(curr);
       return;
     }
 
     if (curr.length < comboLength) {
-      backtrack(curr + '(', leftCount + 1);
-      if (leftCount - 1 >= 0) backtrack(curr + ')', leftCount - 1);
+      if (leftCount < n) backtrack(curr + "(", leftCount + 1);
+      if (leftCount > rightCount) backtrack(curr + ')', leftCount - 1);
     }
     
   }
 
-  backtrack('(', 1);
+  backtrack('(', 1, 0);
   return combinations;
 }
 
