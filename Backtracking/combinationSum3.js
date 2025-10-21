6b3729cd0bbd63c698fa5bf8ef8c153ce801fda1
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 14:05:15 -0700

    refactor to iterate forwards

--- Backtracking/combinationSum3.js
+++ Backtracking/combinationSum3.js
@@ -1,28 +1,28 @@
 const combinationSum3 = (k, n) => {
   const combinations = [];
+  const largestPossibleNum = n < 10 ? n : 9;
+
   const backtrack = (curr, currSum, i) => {
     if (curr.length === k) {
       if (currSum === n) combinations.push([...curr]);
       return;
     }
 
-    for (i; i > 0; i--) {
+    for (i; i <= largestPossibleNum; i++) {
       const newSum = currSum + i;
       if (newSum <= n) {
         curr.push(i);
-        backtrack(curr, newSum, i - 1);
+        backtrack(curr, newSum, i + 1);
         curr.pop();
       }
     }
   };
 
-  const largestPossibleNum = n < 10 ? n : 9;
-
-  backtrack([], 0, largestPossibleNum);
+  backtrack([], 0, 1);
   return combinations;
 };
 
-const k = 4,
-  n = 1;
+const k = 3,
+  n = 9;
 
 console.log(combinationSum3(k, n));
