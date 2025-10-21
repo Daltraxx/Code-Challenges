d43d975b26c5e9ebc63fea9f6635b4f51bd3bdec
Dalton Pettus <daltpettus@gmail.com>
2025-10-20 17:47:30 -0700

    refactor

--- Backtracking/NumsSameConsecDiff.java
+++ Backtracking/NumsSameConsecDiff.java
@@ -6,35 +6,33 @@ import java.util.List;
 
 public class NumsSameConsecDiff {
   public int[] numsSameConsecDiff(int n, int k) {
-    List<Integer> validInts = new ArrayList<>();
     Deque<Integer> queue = new ArrayDeque<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-    while (!queue.isEmpty()) {
-      int num = queue.removeFirst();
-      String numString = Integer.toString(num);
-      if (numString.length() == n) {
-        validInts.add(Integer.parseInt(numString));
-        continue;
-      }
-      int lastDigit = num % 10;
-      if (k == 0) {
-        int newNum = num * 10 + lastDigit;
-        queue.add(newNum);
-        continue;
-      }
-      
-      if (lastDigit + k < 10) {
-        int newNum = num * 10 + lastDigit + k;
-        queue.add(newNum);
-      }
+    for (int level = 1; level < n; level++) {
+      int size = queue.size();
+      for (int node = 0; node < size; node++) {
+        int num = queue.removeFirst();
+        int lastDigit = num % 10;
 
-      if (lastDigit - k >= 0) {
-        int newNum = num * 10 + lastDigit - k;
-        queue.add(newNum);
+        if (k == 0) {
+          int newNum = num * 10 + lastDigit;
+          queue.add(newNum);
+          continue;
+        }
+
+        if (lastDigit + k < 10) {
+          int newNum = num * 10 + lastDigit + k;
+          queue.add(newNum);
+        }
+
+        if (lastDigit - k >= 0) {
+          int newNum = num * 10 + lastDigit - k;
+          queue.add(newNum);
+        }
       }
     }
 
-    int[] ans = validInts.stream().mapToInt(Integer::intValue).toArray();
+    int[] ans = queue.stream().mapToInt(Integer::intValue).toArray();
 
     return ans;
   }
