b70e14e4895e6000247d7c917bebecd7fea445e6
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 13:53:14 -0700

    fix mistake

--- Backtracking/combinationSum3.js
+++ Backtracking/combinationSum3.js
@@ -1,23 +1,26 @@
-const combinationSUm3 = (k, n) => {
+const combinationSum3 = (k, n) => {
   const combinations = [];
   const backtrack = (curr, currSum, i) => {
     if (curr.length === k) {
-      if (currSum === n) {
-        combinations.push([...curr]);
-        return;
-      }
+      if (currSum === n) combinations.push([...curr]);
+      return;
+    }
 
-      for (i; i > 0; i--) {
-        const newSum = currSum + i;
-        if (newSum <= n) {
-          curr.push(i);
-          backtrack(curr, newSum, i - 1);
-          curr.pop();
-        }
+    for (i; i > 0; i--) {
+      const newSum = currSum + i;
+      if (newSum <= n) {
+        curr.push(i);
+        backtrack(curr, newSum, i - 1);
+        curr.pop();
       }
     }
-  }
+  };
 
   backtrack([], 0, n);
   return combinations;
-}
+};
+
+const k = 3,
+  n = 7;
+
+console.log(combinationSum3(k, n));
