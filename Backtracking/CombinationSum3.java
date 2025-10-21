ee086b91b3829de76c3e233be60f29472bc5ccb3
Dalton Pettus <daltpettus@gmail.com>
2025-10-21 14:43:49 -0700

    java solution

---
+++ Backtracking/CombinationSum3.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum3 {
+  int target;
+  int n;
+  List<List<Integer>> combinations;
+  int largestPossibleNum;
+
+  public List<List<Integer>> combinationSum3(int k, int n) {
+    target = k;
+    this.n = n;
+    combinations = new ArrayList<>();
+    largestPossibleNum = n < 10 ? n : 9;
+    backtrack(new ArrayList<>(), 1, 0);
+  }
+  
+  private void backtrack(List<Integer> curr, int i, int currSum) {
+    if (curr.size() == n) {
+      if (currSum == target)
+        combinations.add(new ArrayList<>(curr));
+      return;
+    }
+
+    for (int j = i; j < largestPossibleNum; j++) {
+      int newSum = currSum + j;
+      if (newSum <= target) {
+        curr.add(j);
+        backtrack(curr, j, newSum);
+        curr.remove(curr.size() - 1);
+      } else {
+        return;
+      }
+    }
+  }
+}
