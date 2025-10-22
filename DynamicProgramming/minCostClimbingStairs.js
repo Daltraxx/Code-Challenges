8b03a9a6adbe8a4f78d47fb7db870ced98bd18d8
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 09:21:18 -0700

    fix

--- DynamicProgramming/minCostClimbingStairs.js
+++ DynamicProgramming/minCostClimbingStairs.js
@@ -4,8 +4,12 @@ const minCostClimbingStairs = (cost) => {
       return 0;
     }
 
-    return Math.min(dp(i - 1), dp(i - 2));
+
+    return Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]);
   }
 
   return dp(cost.length);
 }
+
+const cost = [10, 15, 20];
+console.log(minCostClimbingStairs(cost));
