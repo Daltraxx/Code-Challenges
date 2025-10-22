e014a4e76e3b7db792420419d1bf54b4be6095d6
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 11:52:23 -0700

    constant space solution

--- DynamicProgramming/minCostClimbingStairs.js
+++ DynamicProgramming/minCostClimbingStairs.js
@@ -30,6 +30,19 @@ const minCostClimbingStairsBottomUp = (cost) => {
   }
 
   return dp.at(-1);
-}
+};
+
+const minCostClimbingStairsBottomUpConstantSpace = (cost) => {
+  let downOne = 0;
+  let downTwo = 0;
+
+  for (let i = 2; i < cost.length + 1; i++) {
+    const temp = downOne;
+    downOne = Math.min(downOne + cost[i - 1], downTwo + cost[i - 2]);
+    downTwo = temp;
+  }
+
+  return downOne;
+};
 
 console.log(minCostClimbingStairsBottomUp(cost));
