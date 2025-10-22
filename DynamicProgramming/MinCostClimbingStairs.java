50141d7d586018948c1ed655bb596be50d806ce5
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 11:41:28 -0700

    java bottom up solution

--- DynamicProgramming/MinCostClimbingStairs.java
+++ DynamicProgramming/MinCostClimbingStairs.java
@@ -7,7 +7,7 @@ public class MinCostClimbingStairs {
   int[] cost;
   Map<Integer, Integer> memo;
 
-  public int minCostClimbingStairs(int[] cost) {
+  public int minCostClimbingStairsBottomUp(int[] cost) {
     this.cost = cost;
     this.memo = new HashMap<>();
     return dp(cost.length);
@@ -20,15 +20,27 @@ public class MinCostClimbingStairs {
 
     if (memo.containsKey(i))
       return memo.get(i);
-    
+
     memo.put(i, Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]));
 
     return memo.get(i);
   }
 
+  // Top down solution
+  public int minCostClimbingStairsTopDown(int[] cost) {
+    int[] dp = new int[cost.length + 1];
+
+    for (int i = 2; i < dp.length; i++) {
+      dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    return dp[dp.length - 1];
+  }
+
   public static void main(String[] args) {
     MinCostClimbingStairs minCostClimbingStairs = new MinCostClimbingStairs();
-    int[] cost = {10, 15, 20};
-    System.out.println(minCostClimbingStairs.minCostClimbingStairs(cost)); // Output: 15
+    int[] cost = { 10, 15, 20 };
+    System.out.println(minCostClimbingStairs.minCostClimbingStairsBottomUp(cost)); // Output: 15
+    System.out.println(minCostClimbingStairs.minCostClimbingStairsTopDown(cost)); // Output: 15
   }
 }
