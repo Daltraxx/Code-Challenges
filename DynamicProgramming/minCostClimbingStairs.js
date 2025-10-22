12115280cf14d048ab4846064975a988a57b5529
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 09:57:20 -0700

    add memoization

--- DynamicProgramming/minCostClimbingStairs.js
+++ DynamicProgramming/minCostClimbingStairs.js
@@ -1,15 +1,21 @@
 const minCostClimbingStairs = (cost) => {
+  const runningCosts = new Array(cost.length + 1);
   const dp = (i) => {
     if (i === 0 || i === 1) {
-      return 0;
+      runningCosts[i] = 0;
+      return runningCosts[i];
     }
 
+    if (!runningCosts[i])
+      runningCosts[i] = Math.min(
+        Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2])
+      );
 
-    return Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]);
-  }
+    return runningCosts[i];
+  };
 
   return dp(cost.length);
-}
+};
 
-const cost = [10, 15, 20];
+const cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
 console.log(minCostClimbingStairs(cost));
