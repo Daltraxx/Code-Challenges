c5182a3ce76af5e22833c33bac5ac8e291459560
Dalton Pettus <daltpettus@gmail.com>
2025-10-22 11:34:25 -0700

    add memoization

--- DynamicProgramming/MinCostClimbingStairs.java
+++ DynamicProgramming/MinCostClimbingStairs.java
@@ -1,10 +1,15 @@
 package DynamicProgramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinCostClimbingStairs {
   int[] cost;
+  Map<Integer, Integer> memo;
 
   public int minCostClimbingStairs(int[] cost) {
     this.cost = cost;
+    this.memo = new HashMap<>();
     return dp(cost.length);
   }
 
@@ -13,6 +18,17 @@ public class MinCostClimbingStairs {
       return 0;
     }
 
-    return Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]);
+    if (memo.containsKey(i))
+      return memo.get(i);
+    
+    memo.put(i, Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]));
+
+    return memo.get(i);
+  }
+
+  public static void main(String[] args) {
+    MinCostClimbingStairs minCostClimbingStairs = new MinCostClimbingStairs();
+    int[] cost = {10, 15, 20};
+    System.out.println(minCostClimbingStairs.minCostClimbingStairs(cost)); // Output: 15
   }
 }
