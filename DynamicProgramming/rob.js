257b8e252eef4c431ea8382a567eaec1bdd9bab6
Dalton Pettus <daltpettus@gmail.com>
2025-10-23 12:04:55 -0700

    add bottom up approach

--- DynamicProgramming/rob.js
+++ DynamicProgramming/rob.js
@@ -1,4 +1,4 @@
-const rob = (nums) => {
+const robTopDown = (nums) => {
   const memo = new Map();
   const maxMoney = (i) => {
     if (i === 0) {
@@ -25,5 +25,17 @@ const rob = (nums) => {
 // Space O(n)
 
 const nums = [2, 7, 9, 3, 1];
+console.log(robTopDown(nums));
 
-console.log(rob(nums));
+const robBottomUp = (nums) => {
+  const maxMoney = new Array(nums.length);
+  maxMoney[0] = nums[0];
+  maxMoney[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < maxMoney.length; i++) {
+    maxMoney[i] = Math.max(maxMoney[i - 2] + nums[i], maxMoney[i - 1]);
+  }
+
+  return maxMoney.at(-1);
+}
+
+console.log(robBottomUp(nums));
