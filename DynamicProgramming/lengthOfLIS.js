6e7fe437c2d23994d11b6873d262b171c8df1aba
Dalton Pettus <daltpettus@gmail.com>
2025-10-24 12:27:11 -0700

    bottom up solution

--- DynamicProgramming/lengthOfLIS.js
+++ DynamicProgramming/lengthOfLIS.js
@@ -1,4 +1,4 @@
-const lengthOfLIS = (nums) => {
+const lengthOfLISTopDown = (nums) => {
   const memo = new Map();
 
   const getLongestSubsequence = (i) => {
@@ -28,4 +28,20 @@ const lengthOfLIS = (nums) => {
 }
 
 const nums = [1, 3, 6, 7, 9, 4, 10, 5, 6];
-console.log(lengthOfLIS(nums));
+console.log(lengthOfLISTopDown(nums));
+
+const lengthOfLISBottomUp = (nums) => {
+  const longestSubsequences = new Array(nums.length).fill(1);
+  let longest = 1;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        longestSubsequences[i] = Math.max(longestSubsequences[j] + 1, longestSubsequences[i]);
+        longest = Math.max(longestSubsequences[i], longest);
+      }
+    }
+  }
+  return longest;
+}
+
+console.log(lengthOfLISBottomUp(nums));
