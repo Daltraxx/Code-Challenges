559512ac7fe9fa74cd0158a40ddd90ea05e0a463
Dalton Pettus <daltpettus@gmail.com>
2025-10-27 10:47:31 -0700

    documentation

--- DynamicProgramming/mostPoints.js
+++ DynamicProgramming/mostPoints.js
@@ -13,10 +13,10 @@ const mostPoints = (questions) => {
     const [points, questionsToSkip] = questions[i];
     memo[i] = Math.max(points + dp(i + questionsToSkip + 1), dp(i + 1));
     return memo[i];
-  }
+  };
 
   return dp(0);
-}
+};
 
 const questions = [
   [3, 2],
@@ -29,14 +29,17 @@ console.log(mostPoints(questions));
 // BOTTOM-UP
 const mostPointsBottomUp = (questions) => {
   const n = questions.length;
-  const dp = new Array(n + 1).fill(0);
+  const dp = new Array(n + 1).fill(0); // n + 1 to avoid out of bounds
   for (let i = n - 1; i >= 0; i--) {
     const [points, questionsToSkip] = questions[i];
     let nextAnswerableQuestion = i + questionsToSkip + 1;
-    dp[i] = Math.max(points + dp[Math.min(nextAnswerableQuestion, n)], dp[i + 1]);
+    dp[i] = Math.max(
+      points + dp[Math.min(nextAnswerableQuestion, n)], // make sure don't go over n
+      dp[i + 1]
+    );
   }
 
   return dp[0];
-}
+};
 
 console.log(mostPointsBottomUp(questions));
