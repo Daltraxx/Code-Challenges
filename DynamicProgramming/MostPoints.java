fb341a64e64972143c61bd9780782f23466e8b17
Dalton Pettus <daltpettus@gmail.com>
2025-10-28 10:19:58 -0700

    bottom up solution

--- DynamicProgramming/MostPoints.java
+++ DynamicProgramming/MostPoints.java
@@ -26,4 +26,19 @@ public class MostPoints {
     memo[i] = Math.max(points + dp(i + questionsToSkip + 1), dp(i + 1));
     return memo[i];
   }
+
+  // BOTTOM UP APPROACH
+  public long mostPointsBottomUp(int[][] questions) {
+    int n = questions.length;
+    long[] dp = new long[n + 1];
+    dp[n] = 0;
+    for (int i = n - 1; i >= 0; i--) {
+      int points = questions[i][0];
+      int questionsToSkip = questions[i][1];
+      int j = Math.min(i + questionsToSkip + 1, n);
+      dp[i] = Math.max(points + dp[j], dp[i + 1]);
+    }
+
+    return dp[0];
+  }
 }
