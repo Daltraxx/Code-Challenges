5cafbec8751550f3ecbecd86351f6640bfaffd8f
Dalton Pettus <daltpettus@gmail.com>
2025-10-28 10:22:45 -0700

    add comments

--- DynamicProgramming/MostPoints.java
+++ DynamicProgramming/MostPoints.java
@@ -31,12 +31,12 @@ public class MostPoints {
   // BOTTOM UP APPROACH
   public long mostPointsBottomUp(int[][] questions) {
     int n = questions.length;
-    long[] dp = new long[n + 1];
+    long[] dp = new long[n + 1]; // n + 1 to avoid out of bounds
     dp[n] = 0;
     for (int i = n - 1; i >= 0; i--) {
       int points = questions[i][0];
       int questionsToSkip = questions[i][1];
-      int j = Math.min(i + questionsToSkip + 1, n);
+      int j = Math.min(i + questionsToSkip + 1, n); // n as safeguard to make sure we don't go out of bounds
       dp[i] = Math.max(points + dp[j], dp[i + 1]);
     }
 
