b3b661f8c5d4383d76d86036e4a6895d89860ad3
Dalton Pettus <daltpettus@gmail.com>
2025-10-28 10:02:00 -0700

    add memoization

--- DynamicProgramming/MostPoints.java
+++ DynamicProgramming/MostPoints.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
+
 public class MostPoints {
   // TOP DOWN APPROACH
   int[][] questions;
+  long[] memo;
 
   public long mostPoints(int[][] questions) {
     this.questions = questions;
+    memo = new long[questions.length];
+    Arrays.fill(memo, -1);
     return dp(0);
   }
 
@@ -11,8 +16,14 @@ public class MostPoints {
     if (i >= questions.length) {
       return 0;
     }
+
+    if (memo[i] != -1) {
+      return memo[i];
+    }
+
     int points = questions[i][0];
     int questionsToSkip = questions[i][1];
-    return Math.max(points + dp(i + questionsToSkip + 1), dp(i + 1));
+    memo[i] = Math.max(points + dp(i + questionsToSkip + 1), dp(i + 1));
+    return memo[i];
   }
 }
