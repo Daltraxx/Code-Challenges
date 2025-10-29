37c804ce56bdaac7ced3c26d52a5cd933358b0de
Dalton Pettus <daltpettus@gmail.com>
2025-10-29 08:25:07 -0700

    dp top down solution

--- DynamicProgramming/climbStairs.js
+++ DynamicProgramming/climbStairs.js
@@ -1,4 +1,4 @@
-const climbStairs = (n) => {
+const climbStairsBacktracking = (n) => {
   let ways = 0;
   const backtrack = (step) => {
     if (step === n) {
@@ -20,4 +20,21 @@ const climbStairs = (n) => {
 // Space O(n)
 
 const n = 3;
+console.log(climbStairsBacktracking(n));
+
+// TOP-DOWN
+const climbStairs = (n) => {
+  const getWaysToReachStep = (step) => {
+    if (step === 2) {
+      return 2;
+    } else if (step == 1) {
+      return 1;
+    }
+
+    return 2 + getWaysToReachStep(n - 2);
+  }
+
+  return getWaysToReachStep(n);
+}
+
 console.log(climbStairs(n));
