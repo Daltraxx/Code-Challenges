887b97b80cdcb92f3405704648a19cf7290da212
Dalton Pettus <daltpettus@gmail.com>
2025-10-30 04:25:32 -0700

    coin change backtracking brute force attempt

---
+++ DynamicProgramming/coinChange.js
@@ -0,0 +1,19 @@
+const coinChange = (coins, amount) => {
+  coins.sort((a, b) => b - a);
+  const backtrack = (i, sum, coinCount) => {
+    for (i; i < coins.length; i++) {
+      if (sum + coins[i] < amount) {
+        let result = backtrack(i, sum + coins[i], coinCount + 1);
+        if (result !== -1) return result;
+      }
+    }
+
+    return -1;
+  }
+
+  return backtrack(0, 0, 0);
+}
+
+const coins = [1, 2, 5],
+  amount = 11;
+console.log(coinChange(coins, amount));
