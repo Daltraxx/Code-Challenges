8fca84fc86078f47e54e7867a1d77cc9dc3b0ae3
Dalton Pettus <daltpettus@gmail.com>
2026-01-17 10:35:23 -0800

    remove brute force solution

--- DynamicProgramming/coinChange.js
+++ DynamicProgramming/coinChange.js
@@ -1,27 +1,3 @@
-const coinChangeBacktracking = (coins, amount) => {
-  if (amount === 0) return 0;
-  coins.sort((a, b) => b - a);
-  const backtrack = (i, sum, coinCount) => {
-    if (sum === amount) {
-      return coinCount;
-    }
-    for (i; i < coins.length; i++) {
-      if (sum + coins[i] <= amount) {
-        let result = backtrack(i, sum + coins[i], coinCount + 1);
-        if (result !== -1) return result;
-      }
-    }
-
-    return -1;
-  };
-
-  return backtrack(0, 0, 0);
-};
-
-const coins = [1, 2, 5],
-  amount = 11;
-console.log(coinChangeBacktracking(coins, amount));
-
 // TOP-DOWN
 // Recurrence relation: dp(amount) = 1 + min(amount - coin)
 // where we try all possible coins and pick the best option
