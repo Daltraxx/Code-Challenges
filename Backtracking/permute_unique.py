25a6e79d28ab98acd250f8345d4a4fc05f3e0f24
Dalton Pettus <daltpettus@gmail.com>
2026-01-18 12:25:00 -0800

    improve algorithm

--- Backtracking/permute_unique.py
+++ Backtracking/permute_unique.py
@@ -3,11 +3,8 @@ import hashlib
 
 
 class PermuteUnique:
-    
-  
   def permute_unique(self, nums: List[int]) -> List[List[int]]:
     permutations = []
-    used_permutations = set()
     counts = dict()
     for num in nums:
       counts[num] = counts.get(num, 0) + 1
@@ -15,13 +12,10 @@ class PermuteUnique:
 
     def backtrack(curr: List[int], curr_counts: dict[int, int]):
       if len(curr) == len(nums):
-        permutation = tuple(curr)
-        if permutation not in used_permutations:
-          permutations.append([*curr])
-          used_permutations.add(permutation)
+        permutations.append(curr[:])
         return
-      for num in nums:
-        if num not in curr_counts or curr_counts[num] < counts[num]:
+      for num in counts:
+        if curr_counts.get(num, 0) < counts[num]:
           curr_counts[num] = curr_counts.get(num, 0) + 1
           curr.append(num)
           backtrack(curr, curr_counts)
