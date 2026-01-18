24ac3c63bc80f6c6e983f6a7380944c34540aa11
Dalton Pettus <daltpettus@gmail.com>
2026-01-18 12:32:01 -0800

    improve with counter ds

--- Backtracking/permute_unique.py
+++ Backtracking/permute_unique.py
@@ -1,28 +1,25 @@
 from typing import List
-import hashlib
+from collections import Counter
 
 
 class PermuteUnique:
   def permute_unique(self, nums: List[int]) -> List[List[int]]:
     permutations = []
-    counts = dict()
-    for num in nums:
-      counts[num] = counts.get(num, 0) + 1
 
 
-    def backtrack(curr: List[int], curr_counts: dict[int, int]):
+    def backtrack(curr: List[int], counter: Counter):
       if len(curr) == len(nums):
         permutations.append(curr[:])
         return
-      for num in counts:
-        if curr_counts.get(num, 0) < counts[num]:
-          curr_counts[num] = curr_counts.get(num, 0) + 1
+      for num in counter:
+        if counter[num] > 0:
+          counter[num] -=1
           curr.append(num)
-          backtrack(curr, curr_counts)
-          curr_counts[num] -= 1
+          backtrack(curr, counter)
+          counter[num] += 1
           curr.pop()
     
-    backtrack([], dict())
+    backtrack([], Counter(nums))
     
     return permutations
       
