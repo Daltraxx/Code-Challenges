8f5396fc5fd846ba3ad07420f5a1e8d6b4112c3a
Dalton Pettus <daltpettus@gmail.com>
2026-01-19 12:00:15 -0800

    ReversePrefix python two pointer attempt

---
+++ Two Pointer/reverse_prefix.py
@@ -0,0 +1,18 @@
+class ReversePrefix:
+  def reversePrefix(self, word: str, ch: str) -> str:
+    left = 0
+    right = word.find(ch) # -1 if not found
+    wordList = list(word)
+    while left < right:
+      temp = wordList[left]
+      wordList[left] = wordList[right]
+      wordList[right] = temp
+      left += 1
+      right -= 1
+    
+    return "".join(wordList)
+
+word = "abcdefd"
+ch = "z"
+
+print(ReversePrefix().reversePrefix(word, ch))
