2f09606dca0a18cea8fbba0f47672b608470741f
Dalton Pettus <daltpettus@gmail.com>
2026-01-19 14:35:31 -0800

    refactor

--- Two Pointer/reverse_prefix.py
+++ Two Pointer/reverse_prefix.py
@@ -1,18 +1,20 @@
 class ReversePrefix:
   def reversePrefix(self, word: str, ch: str) -> str:
     left = 0
-    right = word.find(ch) # -1 if not found
     wordList = list(word)
-    while left < right:
-      wordList[left], wordList[right] = wordList[right], wordList[left]
-      left += 1
-      right -= 1
-    
-    return "".join(wordList)
+    for right in range(len(word)):
+      if word[right] == ch:
+        while left < right:
+          wordList[left], wordList[right] = wordList[right], wordList[left]
+          left += 1
+          right -= 1
+        return "".join(wordList)
+      
+    return word
 
 # Time complexity: O(n) where n is the length of the word.
 # Space complexity: O(n) for the list conversion of the string.
 word = "abcdefd"
-ch = "z"
+ch = "d"
 
 print(ReversePrefix().reversePrefix(word, ch))
