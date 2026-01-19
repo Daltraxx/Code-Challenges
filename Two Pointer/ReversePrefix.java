5bcc4d18cffc93552524cedbe935912e306a5096
Dalton Pettus <daltpettus@gmail.com>
2026-01-19 15:30:20 -0800

    java attempt

---
+++ Two Pointer/ReversePrefix.java
@@ -0,0 +1,19 @@
+public class ReversePrefix {
+  public String reversePrefix(String word, char ch) {
+    char[] wordArr = word.toCharArray();
+    int left = 0;
+    for (int right = 0; right < word.length(); right++) {
+      if (word.charAt(right) == ch) {
+        while (left < right) {
+          char temp = wordArr[left];
+          wordArr[left] = wordArr[right];
+          wordArr[right] = temp;
+          left++;
+          right++;
+        }
+        return new String(wordArr);
+      }
+    }
+    return word;
+  }
+}
