4fc94b9e54f706321b4ceaef63c1b74fec3d5af4
Dalton Pettus <daltpettus@gmail.com>
2026-01-20 12:24:38 -0800

    remove object param

--- DynamicProgramming/coin_change.py
+++ DynamicProgramming/coin_change.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 from typing import List
 
-class CoinChangeTopDown(object):
+class CoinChangeTopDown:
   def coinChange(self, coins: List[int], amount: int) -> int:
     @lru_cache(None)
     def get_min_coins(amount):
@@ -21,7 +21,7 @@ class CoinChangeTopDown(object):
 
 print(CoinChangeTopDown().coinChange([1, 2, 5], 11))  # Output: 3
 
-class CoinChangeBottomUp(object):
+class CoinChangeBottomUp:
   def coinChange(self, coins: List[int], amount: int) -> int:
     min_coins_per_amount = [float("inf")] * (amount + 1)
     min_coins_per_amount[0] = 0
