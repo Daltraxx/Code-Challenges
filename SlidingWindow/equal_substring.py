dfcc379d0a5b1d2a48c3f1d74e8c61a911fef96e
Dalton Pettus <daltpettus@gmail.com>
2026-01-21 12:59:59 -0800

    formatting

--- SlidingWindow/equal_substring.py
+++ SlidingWindow/equal_substring.py
@@ -1,23 +1,24 @@
 class EqualSubstring:
-  def equal_substring(self, s: str, t:str, maxCost: int) -> int:
-    currentCost = 0
-    maxLength = 0
-    left = 0
-    for right in range(len(s)):
-      currentCost += self.getCost(s[right], t[right])
-      while (currentCost > maxCost):
-        currentCost -= self.getCost(s[left], t[left])
-        left +=1
-      maxLength = max(right - left + 1, maxLength)
-    return maxLength
-  
-  @staticmethod
-  def getCost(a: str, b: str) -> int:
-    return abs(ord(a) - ord(b))
-  
+    def equal_substring(self, s: str, t: str, maxCost: int) -> int:
+        currentCost = 0
+        maxLength = 0
+        left = 0
+        for right in range(len(s)):
+            currentCost += self.getCost(s[right], t[right])
+            while currentCost > maxCost:
+                currentCost -= self.getCost(s[left], t[left])
+                left += 1
+            maxLength = max(right - left + 1, maxLength)
+        return maxLength
+
+    @staticmethod
+    def get_cost(a: str, b: str) -> int:
+        return abs(ord(a) - ord(b))
+
+
 # Time Complexity: O(n)
 # Space Complexity: O(1)
-  
+
 s = "abcd"
 t = "bcdf"
 maxCost = 3
