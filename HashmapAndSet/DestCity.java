9c0b51c25912b202f2b4efb8bc24eb16bbee97cf
Dalton Pettus <daltpettus@gmail.com>
2026-01-24 11:03:50 -0800

    java solution

---
+++ HashmapAndSet/DestCity.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DestCity {
+  public String destCity(List<List<String>> paths) {
+    Set<String> hasOutgoing = new HashSet<>();
+    for (List<String> edge : paths) {
+      hasOutgoing.add(edge.get(0));
+    }
+
+    for (List<String> edge : paths) {
+      if (!hasOutgoing.contains(edge.get(1)))
+        return edge.get(1);
+    }
+    
+    return "";
+  }
+}
+
+// Time Complexity: O(N), where N is the number of paths
+// Space Complexity: O(N) for the HashSet
