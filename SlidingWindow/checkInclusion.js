a96fbd7ecf8843401d56688826ab7524e968c6e3
Dalton Pettus <daltpettus@gmail.com>
2026-01-26 11:39:01 -0800

    js checkInclusion attempt

---
+++ SlidingWindow/checkInclusion.js
@@ -0,0 +1,34 @@
+const checkInclusion = (s1, s2) => {
+  const reqCharCounts = new Map();
+  const reqCharsInWindow = new Map();
+  const distinctCharsRemaining = 0;
+
+  for (let char of s1) {
+    reqCharCounts.set(char, (reqCharCounts.get(char) || 0) + 1);
+    if (reqCharCounts.get(char) === 1) distinctCharsRemaining++;
+  }
+
+  let left = 0;
+  for (let right = 0; right < s2.length; right++) {
+    const rightChar = s2[right];
+    if (reqCharCounts.get(rightChar)) {
+      reqCharsInWindow.set(rightChar, (reqCharsInWindow.get(char) || 0) + 1);
+      if (reqCharCounts.get(rightChar) === reqCharsInWindow.get(rightChar))
+        distinctCharsRemaining--;
+    }
+
+    // Maintain valid window size
+    if (right - left + 1 > s1.length) {
+      const leftChar = s2[left];
+      if (reqCharsInWindow.has(leftChar)) {
+        reqCharsInWindow.set(leftChar, reqCharsInWindow.get(leftChar) - 1);
+        if (reqCharsInWindow.get(leftChar) === reqCharCounts.get(leftChar) - 1)
+          distinctCharsRemaining++;
+      }
+    }
+
+    if (distinctCharsRemaining === 0) return true;
+  }
+
+  return false;
+};
