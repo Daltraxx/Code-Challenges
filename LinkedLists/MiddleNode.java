c02c2b07e91414ec1f167180f865103f98b8411f
Dalton Pettus <daltpettus@gmail.com>
2026-01-29 11:19:50 -0800

    java solution

--- LinkedLists/MiddleNode.java
+++ LinkedLists/MiddleNode.java
@@ -1,5 +1,17 @@
 package LinkedLists;
 
 public class MiddleNode {
-  
+  public ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  }
 }
+
+// Time Complexity: O(N) where N is the number of nodes in the linked list.
+// Space Complexity: O(1) as we are using only constant extra space.
