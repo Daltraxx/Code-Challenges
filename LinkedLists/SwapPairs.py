1741e6111ff348ddb271099be6292e9b5571360f
Dalton Pettus <daltpettus@gmail.com>
2026-01-30 11:32:20 -0800

    Swap Nodes in Pairs python solution

---
+++ LinkedLists/SwapPairs.py
@@ -0,0 +1,26 @@
+from list_node import ListNode
+
+
+class SwapPairs:
+    def swap_pairs(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+
+        newHead = head.next
+        prev = None
+        current = head
+        while current and current.next:
+            if prev:
+                prev.next = current.next
+
+            prev = current
+            next = current.next.next
+            current.next.next = current
+            current.next = next
+            current = next
+
+        return newHead
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
