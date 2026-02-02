60dbceb6182e00696ddc2bb48346027931a8cdf7
Dalton Pettus <daltpettus@gmail.com>
2026-02-02 12:00:53 -0800

    reverse linked list II java solution

---
+++ LinkedLists/ReverseBetween.java
@@ -0,0 +1,38 @@
+package LinkedLists;
+
+public class ReverseBetween {
+  public ListNode reverseBetween(ListNode head, int left, int right) {
+    // Find node at left position (1-indexed)
+    ListNode current = head;
+    ListNode prevLeft = null;
+    for (int i = 1; i < left; i++) {
+      prevLeft = current;
+      current = current.next;
+    }
+
+    // Perform reversal
+    ListNode reversedTail = current;
+    ListNode prevNode = null;
+    for (int i = left; i < right + 1; i++) {
+      ListNode nextNode = current.next;
+      current.next = prevNode;
+      prevNode = current;
+      current = nextNode;
+    }
+
+    // Connect head and tail of reversed segment to larger list
+    ListNode reversedHead = prevNode;
+    if (prevLeft != null) {
+      prevLeft.next = reversedHead;
+    } else {
+      head = reversedHead;
+    }
+
+    reversedTail.next = current;
+
+    return head;
+  }
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
