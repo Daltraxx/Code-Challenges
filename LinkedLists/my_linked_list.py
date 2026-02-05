2e73b21c7eecc38404129e4cc803d9a96cf04c50
Dalton Pettus <daltpettus@gmail.com>
2026-02-05 10:46:46 -0800

    refactor with size property

--- LinkedLists/my_linked_list.py
+++ LinkedLists/my_linked_list.py
@@ -4,73 +4,75 @@ from list_node import ListNode
 class MyLinkedList:
     def __init__(self, head: ListNode = None):
         self.head = head
+        self.size = 0
+        if head:
+            current = head
+            while current:
+                self.size += 1
+                current = current.next
 
     def get(self, index: int) -> int:
+        if index >= self.size:
+            return -1
         current = self.head
         for _ in range(index):
-            if not current:
-                return -1
             current = current.next
-        return current.val if current else -1
+        return current.val
 
     def addAtHead(self, val: int) -> None:
+        self.size += 1
         newHead = ListNode(val, self.head)
         self.head = newHead
 
     def addAtTail(self, val: int) -> None:
+        self.size += 1
         newNode = ListNode(val)
-        if not self.head:
-            self.head = newNode
-            return
-
-        current = self.head
+        sentinel = ListNode(0, self.head)
+        current = sentinel
         while current.next:
             current = current.next
-
         current.next = newNode
+        # Update head if necessary
+        self.head = sentinel.next
 
     def addAtIndex(self, index: int, val: int) -> None:
+        if index > self.size or index < 0:
+            return
+        self.size += 1
         newNode = ListNode(val)
         sentinel = ListNode(0, self.head)
         prev = sentinel
         current = self.head
-        i = 0
-        while current and i < index:
+        for _ in range(index):
             prev = current
             current = current.next
-            i += 1
-        if i == index:
-            prev.next = newNode
-            newNode.next = current
-        if sentinel.next == newNode:
-            self.head = newNode
+        prev.next = newNode
+        newNode.next = current
+        # Update head if necessary
+        self.head = sentinel.next
 
     def deleteAtIndex(self, index: int) -> None:
+        if index >= self.size or index < 0:
+            return
+        self.size -= 1
         sentinel = ListNode(0, self.head)
         prev = sentinel
         current = self.head
-        i = 0
-        while current and i < index:
+        for _ in range(index):
             prev = current
             current = current.next
-            i += 1
-        if i == index:
-            if current:
-                prev.next = current.next
-            else:
-                # At tail
-                prev.next = None
 
+        prev.next = current.next
         self.head = sentinel.next
 
     def printList(self) -> None:
         current = self.head
-        listAsString = ""
+        nodeList = []
         while current:
-            listAsString += str(current.val) + "->"
+            nodeList.append(f"{current.val} -> ")
             current = current.next
-        listAsString += "None"
-        print(listAsString)
+        nodeList.append("None")
+        print("".join(nodeList))
 
 
 linkedList = MyLinkedList()
@@ -82,4 +84,4 @@ linkedList.addAtIndex(1, 2)  # linked list becomes 1->2->3
 linkedList.printList()  # 1->2->3->None
 print(linkedList.get(1))  # return 2
 linkedList.deleteAtIndex(1)  # now the linked list is 1->3
-print(linkedList.get(1))  # return 3
+print(linkedList.get(1))  # return 
