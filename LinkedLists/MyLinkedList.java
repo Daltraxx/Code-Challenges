093986ebd6fd3f7016ca8ed9ae2e9228bbda151e
Dalton Pettus <daltpettus@gmail.com>
2026-02-05 14:38:34 -0800

    java solution

---
+++ LinkedLists/MyLinkedList.java
@@ -0,0 +1,78 @@
+package LinkedLists;
+
+public class MyLinkedList {
+  ListNode head;
+  int size;
+
+  public MyLinkedList() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  public MyLinkedList(ListNode head) {
+    this.head = head;
+    this.size = 0;
+    if (head != null) {
+      ListNode current = head;
+      while (current != null) {
+        size++;
+        current = current.next;
+      }
+    }
+  }
+
+  public int get(int index) {
+    if (index < 0 || index >= this.size) {
+      return -1;
+    }
+
+    ListNode current = this.head;
+    for (int i = 0; i < index; i++) {
+      current = current.next;
+    }
+    return current.val;
+  }
+
+  public void addAtHead(int val) {
+    addAtIndex(0, val);
+  }
+
+  public void addAtTail(int val) {
+    addAtIndex(this.size, val);
+  }
+
+  public void addAtIndex(int index, int val) {
+    if (index < 0 || index > this.size) {
+      return;
+    }
+    this.size++;
+
+    ListNode sentinel = new ListNode(0, this.head);
+    ListNode prev = sentinel;
+    ListNode current = this.head;
+    for (int i = 0; i < index; i++) {
+      prev = current;
+      current = current.next;
+    }
+    ListNode newNode = new ListNode(val, current);
+    prev.next = newNode;
+    // Update head if necessary
+    this.head = sentinel.next;
+  }
+
+  public void deleteAtIndex(int index) {
+    if (index < 0 || index >= this.size) {
+      return;
+    }
+    this.size--;
+
+    ListNode sentinel = new ListNode(0, this.head);
+    ListNode prev = sentinel;
+    for (int i = 0; i < index; i++) {
+      prev = prev.next;
+    }
+    prev.next = prev.next.next;
+    // Update head if necessary
+    this.head = sentinel.next;
+  }
+}
