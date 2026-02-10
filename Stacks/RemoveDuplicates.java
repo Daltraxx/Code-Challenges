992dc19f7db495b1402dcf9e2b59b0b15f780580
Dalton Pettus <daltpettus@gmail.com>
2026-02-09 18:48:27 -0800

    refactor

--- Stacks/RemoveDuplicates.java
+++ Stacks/RemoveDuplicates.java
@@ -2,11 +2,13 @@ package Stacks;
 
 public class RemoveDuplicates {
   public String removeDuplicates(String s) {
+    // Stringbuilder can act as a stack, we can append characters and delete the last character when we find a duplicate
     StringBuilder noAdjacentDups = new StringBuilder();
     for (int i = 0; i < s.length(); i++) {
       char character = s.charAt(i);
-      if (noAdjacentDups.length() > 0 && noAdjacentDups.charAt(noAdjacentDups.length() - 1) == character) {
-        noAdjacentDups.deleteCharAt(noAdjacentDups.length() - 1);
+      int noAdjacentDupsLength = noAdjacentDups.length();
+      if (noAdjacentDupsLength > 0 && noAdjacentDups.charAt(noAdjacentDupsLength - 1) == character) {
+        noAdjacentDups.deleteCharAt(noAdjacentDupsLength - 1);
       } else {
         noAdjacentDups.append(character);
       }
@@ -14,3 +16,7 @@ public class RemoveDuplicates {
     return noAdjacentDups.toString();
   }
 }
+
+
+// Time Complexity: O(n) - We traverse the string once, and each operation on the StringBuilder is O(1).
+// Space Complexity: O(n) - In the worst case if there are no adjacent duplicates
