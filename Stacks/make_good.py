9e332dcd5e0a0b96b9218e3fda1f13579dc9e598
Dalton Pettus <daltpettus@gmail.com>
2026-02-11 11:45:36 -0800

    add other solution method

--- Stacks/make_good.py
+++ Stacks/make_good.py
@@ -1,4 +1,4 @@
-class MakeGood:
+class MakeGood1:
     def make_good(self, s: str) -> str:
         stack = []
         for char in s:
@@ -7,6 +7,21 @@ class MakeGood:
             else:
                 stack.append(char)
         return "".join(stack)
+
+
+class MakeGood2:
+    def make_good(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and self.is_bad_pair(stack[-1], char):
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
+
+    @staticmethod
+    def is_bad_pair(char1: str, char2: str) -> bool:
+        return char1 != char2 and char1.lower() == char2.lower()
 
 
 # Time complexity: O(n) where n is the length of the input string s
