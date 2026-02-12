d735b4563b91a8c177c29d64b945755d96f68fb2
Dalton Pettus <daltpettus@gmail.com>
2026-02-12 06:51:01 -0800

    js solution

---
+++ Stacks/makeGood.js
@@ -0,0 +1,20 @@
+const makeGood = (s) => {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    const lastChar = stack[stack.length - 1];
+    if (
+      lastChar &&
+      lastChar !== char &&
+      lastChar.toLowerCase() === char.toLowerCase()
+    ) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.join('');
+};
+
+// Time Complexity: O(n) where n is the length of the input string s
+// Space Complexity: O(n) in the worst case when no characters are removed and all characters are added to the stack
