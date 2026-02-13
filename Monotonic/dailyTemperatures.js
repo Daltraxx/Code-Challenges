bb05204a035cf7990e235284a054dc9c2ac9162f
Dalton Pettus <daltpettus@gmail.com>
2026-02-13 08:57:48 -0800

    formatting

--- Monotonic/dailyTemperatures.js
+++ Monotonic/dailyTemperatures.js
@@ -4,20 +4,24 @@ If there is no future day for which this is possible,
 keep answer[i] == 0 instead.*/
 
 const dailyTemperatures = (temperatures) => {
-    const stack = [];
-    const answer = new Array(temperatures.length).fill(0);
-    for (let i = 0; i < temperatures.length; i++) {
-        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
-            const j = stack.pop();
-            answer[j] = i - j;
-        }
-        stack.push(i);
+  const stack = [];
+  const answer = new Array(temperatures.length).fill(0);
+  for (let i = 0; i < temperatures.length; i++) {
+    while (
+      stack.length &&
+      temperatures[i] > temperatures[stack[stack.length - 1]]
+    ) {
+      const j = stack.pop();
+      answer[j] = i - j;
     }
+    stack.push(i);
+  }
 
-    return answer;
-}
+  return answer;
+};
 
-//linear time and space;
+// Time Complexity: O(n) where n is the length of the input array temperatures. Each element is pushed and popped from the stack at most once.
+// Space Complexity: O(n) in the worst case, when the stack contains all the indices of the input array. The output array answer also takes O(n) space.
 
-const temperatures = [73,74,75,71,69,72,76,73];
+const temperatures = [73, 74, 75, 71, 69, 72, 76, 73];
 console.log(dailyTemperatures(temperatures));
