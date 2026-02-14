aa896e5097b1f5bbd6ef45469257f7ebc1ba0cca
Dalton Pettus <daltpettus@gmail.com>
2026-02-14 12:50:54 -0800

    formatting

--- Monotonic/daily_temperatures.py
+++ Monotonic/daily_temperatures.py
@@ -7,12 +7,16 @@ class DailyTemperatures:
         monoDecreasingStack = []
 
         for i in range(len(temperatures)):
-            while monoDecreasingStack and temperatures[i] > temperatures[monoDecreasingStack[-1]]:
+            while (
+                monoDecreasingStack
+                and temperatures[i] > temperatures[monoDecreasingStack[-1]]
+            ):
                 colderDay = monoDecreasingStack.pop()
                 daysToWarmerTemp[colderDay] = i - colderDay
             monoDecreasingStack.append(i)
 
         return daysToWarmerTemp
+
 
 # Time Complexity: O(n) where n is the number of temperatures, as each temperature is processed at most twice (once when added to the stack and once when removed).
 # Space Complexity: O(n) in the worst case, if the temperatures are in decreasing order, as all temperatures would be added to the stack.
