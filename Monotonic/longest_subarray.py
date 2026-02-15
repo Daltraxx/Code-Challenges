cda6bb57bf3a82040f201cb73a492f9b694215da
Dalton Pettus <daltpettus@gmail.com>
2026-02-15 12:01:15 -0800

    longest continuous subarray with absolute diff less than or equal to limit python solution

---
+++ Monotonic/longest_subarray.py
@@ -0,0 +1,36 @@
+from collections import deque
+from typing import List
+
+
+class LongestSubarray:
+    def longest_subarray(self, nums: List[int], limit: int):
+        monoDecreasing = deque()
+        monoIncreasing = deque()
+        max_window = 0
+
+        left = 0
+        for right in range(len(nums)):
+            num = nums[right]
+
+            while monoDecreasing and num > monoDecreasing[-1]:
+                monoDecreasing.pop()
+            monoDecreasing.append(num)
+
+            while monoIncreasing and num < monoIncreasing[-1]:
+                monoIncreasing.pop()
+            monoIncreasing.append(num)
+
+            # Maintain valid window
+            while monoDecreasing[0] - monoIncreasing[0] > limit:
+                if monoDecreasing[0] == nums[left]:
+                    monoDecreasing.popleft()
+                if monoIncreasing[0] == nums[left]:
+                    monoIncreasing.popleft()
+                left += 1
+
+            max_window = max(right - left + 1, max_window)
+
+        return max_window
+
+# Time Complexity: O(n) where n is the number of elements in the input array nums, as each element is processed at most twice (once when added to the deques and once when removed).
+# Space Complexity: O(n) in the worst case, when the deques contain all the elements of the current window.
