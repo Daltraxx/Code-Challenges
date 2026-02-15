8c0bd64f3fcc610b38a1253862d5ad6d9efe5f4a
Dalton Pettus <daltpettus@gmail.com>
2026-02-15 13:40:47 -0800

    refactor

--- Monotonic/longest_subarray.py
+++ Monotonic/longest_subarray.py
@@ -12,25 +12,26 @@ class LongestSubarray:
         for right in range(len(nums)):
             num = nums[right]
 
-            while monoDecreasing and num > monoDecreasing[-1]:
+            while monoDecreasing and num > nums[monoDecreasing[-1]]:
                 monoDecreasing.pop()
-            monoDecreasing.append(num)
+            monoDecreasing.append(right)
 
-            while monoIncreasing and num < monoIncreasing[-1]:
+            while monoIncreasing and num < nums[monoIncreasing[-1]]:
                 monoIncreasing.pop()
-            monoIncreasing.append(num)
+            monoIncreasing.append(right)
 
             # Maintain valid window
-            while monoDecreasing[0] - monoIncreasing[0] > limit:
-                if monoDecreasing[0] == nums[left]:
-                    monoDecreasing.popleft()
-                if monoIncreasing[0] == nums[left]:
-                    monoIncreasing.popleft()
+            while nums[monoDecreasing[0]] - nums[monoIncreasing[0]] > limit:
                 left += 1
+                if monoDecreasing[0] < left:
+                    monoDecreasing.popleft()
+                if monoIncreasing[0] < left:
+                    monoIncreasing.popleft()
 
             max_window = max(right - left + 1, max_window)
 
         return max_window
+
 
 # Time Complexity: O(n) where n is the number of elements in the input array nums, as each element is processed at most twice (once when added to the deques and once when removed).
 # Space Complexity: O(n) in the worst case, when the deques contain all the elements of the current window.
