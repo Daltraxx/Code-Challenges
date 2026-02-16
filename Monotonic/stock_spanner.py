3bc5d614c7fc7bfcb08262ce2126448da90826c8
Dalton Pettus <daltpettus@gmail.com>
2026-02-16 11:52:47 -0800

    refactor

--- Monotonic/stock_spanner.py
+++ Monotonic/stock_spanner.py
@@ -3,7 +3,7 @@ from typing import List
 
 class StockSpanner:
     def __init__(self):
-        self.mono_decreasing_prices: List[List[int]] = []
+        self.mono_decreasing_prices: List[tuple[int, int]] = []
 
     def next(self, price: int) -> int:
         span = 1
@@ -12,9 +12,10 @@ class StockSpanner:
         ):
             prev_span = self.mono_decreasing_prices.pop()[1]
             span += prev_span
-        self.mono_decreasing_prices.append([price, span])
+        self.mono_decreasing_prices.append((price, span))
         return span
 
 
-# Time complexity of each call to next: O(1) amortized. If you average the time taken by next over n calls, it will be O(1) because each price is pushed and popped at most once from the stack.
+# Time complexity (total): O(n) where n is the number of calls to next. 
+# Amortized time complexity of each call to next: O(1). If you average the time taken by next over n calls, it will be O(1) because each price is pushed and popped at most once from the stack.
 # Space complexity: O(n). In the worst case, if the prices are in strictly decreasing order, all prices will be stored in the stack.
