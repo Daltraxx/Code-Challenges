f1df38603ec5de1bc4ab60a689504db725b15c50
Dalton Pettus <daltpettus@gmail.com>
2026-02-21 10:40:13 -0800

    switch to snake case

--- BinaryTrees/good_nodes.py
+++ BinaryTrees/good_nodes.py
@@ -5,16 +5,21 @@ from tree_node import TreeNode
 
 class GoodNodes:
 
-    def goodNodes(self, root: TreeNode):
+    def good_nodes(self, root: TreeNode):
         def dfs(node: Optional[TreeNode], prev_max_val: int):
             if not node:
                 return 0
 
             good_node_contribution = 1 if node.val >= prev_max_val else 0
             updated_max_val = max(node.val, prev_max_val)
-            return good_node_contribution + dfs(node.left, updated_max_val) + dfs(node.right, updated_max_val)
+            return (
+                good_node_contribution
+                + dfs(node.left, updated_max_val)
+                + dfs(node.right, updated_max_val)
+            )
 
-        return dfs(root, float('-inf'))
+        return dfs(root, float("-inf"))
+
 
 # Time complexity: O(n) where n is the number of nodes in the binary tree. We visit each node once.
 # Space complexity: O(n) in the worst case of a completely unbalanced tree, where the height of the tree is n.
