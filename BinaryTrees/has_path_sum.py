ffe1e7c58386c7f10af884b9e0b3493a8f1fd565
Dalton Pettus <daltpettus@gmail.com>
2026-02-20 21:13:58 -0800

    refactor

--- BinaryTrees/has_path_sum.py
+++ BinaryTrees/has_path_sum.py
@@ -11,9 +11,11 @@ class HasPathSum:
         if not root.left and not root.right:
             return target_sum - root.val == 0
 
-        return self.has_path_sum(root.left, target_sum - root.val) or self.has_path_sum(
-            root.right, target_sum - root.val
-        )
+        valid_left_path = self.has_path_sum(root.left, target_sum - root.val)
+        valid_right_path = self.has_path_sum(root.right, target_sum - root.val)
+
+        return valid_left_path or valid_right_path
+
 
 # Time complexity: O(n) where n is the number of nodes in the binary tree. We visit each node once.
 # Space complexity: O(n) in the worst case of a completely unbalanced tree, where the height of the tree is n.
