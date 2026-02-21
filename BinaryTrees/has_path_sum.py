558cdd2051835ec3338502d5648f34ac80f2e2c0
Dalton Pettus <daltpettus@gmail.com>
2026-02-20 21:11:41 -0800

    change space complexity

--- BinaryTrees/has_path_sum.py
+++ BinaryTrees/has_path_sum.py
@@ -16,4 +16,4 @@ class HasPathSum:
         )
 
 # Time complexity: O(n) where n is the number of nodes in the binary tree. We visit each node once.
-# Space complexity: O(h) where h is the height of the binary tree.
+# Space complexity: O(n) in the worst case of a completely unbalanced tree, where the height of the tree is n.
