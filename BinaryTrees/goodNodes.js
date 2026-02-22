6e2472e18622871b899d934b5e63769ba2f74f4d
Dalton Pettus <daltpettus@gmail.com>
2026-02-22 10:38:56 -0800

    refactor js solution

--- BinaryTrees/goodNodes.js
+++ BinaryTrees/goodNodes.js
@@ -4,47 +4,48 @@ a node X in the tree is named good if in the path from root to X there are no no
 Return the number of good nodes in the binary tree.*/
 
 const goodNodesRecursive = (root) => {
-    const dfs = (node, maxSoFar) => {
-        //if tree/subtree is empty, no good nodes
-        if (!node) {
-            return 0;
-        }
-
-        //count of good nodes will equal the number of good nodes found in the left and right subtrees
-        const left = dfs(node.left, Math.max(maxSoFar, node.val));
-        const right = dfs(node.right, Math.max(maxSoFar, node.val));
-        let goodNodesCount = left + right;
-
-        //if current node is good node, include in count
-        if (node.val >= maxSoFar) goodNodesCount++;
-
-        return goodNodesCount;
+  const dfs = (node, maxSoFar) => {
+    // If tree/subtree is empty, no good nodes
+    if (!node) {
+      return 0;
     }
 
-    return dfs(root, -Infinity);
-}
+    // Count of good nodes will equal the number of good nodes found in the left and right subtrees
+    const updatedMaxSoFar = Math.max(maxSoFar, node.val);
+    const left = dfs(node.left, updatedMaxSoFar);
+    const right = dfs(node.right, updatedMaxSoFar);
+    let goodNodesCount = left + right;
 
-const goodNodesIterative = (root) => {
-    if (!root) {
-        return 0;
-    }
-
-    let goodNodesCount = 0;
-    const stack = [];
-    stack.push([root, -Infinity]);
-
-    while (stack.length) {
-        let [currentNode, maxSoFar] = stack.pop();
-
-        //if current value is greater than prev maxSoFar, good node so increment count
-        if (currentNode.val >= maxSoFar) goodNodesCount++;
-        //keep maxSoFar updated
-        maxSoFar = Math.max(maxSoFar, currentNode.val);
-
-        //push existing children to stack to continue down tree
-        if (currentNode.left) stack.push([currentNode.left, maxSoFar]);
-        if (currentNode.right) stack.push([currentNode.right, maxSoFar]);
-    }
+    // If current node is good node, include in count
+    if (node.val >= maxSoFar) goodNodesCount++;
 
     return goodNodesCount;
-}
+  };
+
+  return dfs(root, -Infinity);
+};
+
+const goodNodesIterative = (root) => {
+  if (!root) {
+    return 0;
+  }
+
+  let goodNodesCount = 0;
+  const stack = [];
+  stack.push([root, -Infinity]);
+
+  while (stack.length) {
+    let [currentNode, maxSoFar] = stack.pop();
+
+    // If current value is greater than prev maxSoFar, good node so increment count
+    if (currentNode.val >= maxSoFar) goodNodesCount++;
+    // Keep maxSoFar updated
+    maxSoFar = Math.max(maxSoFar, currentNode.val);
+
+    // Push existing children to stack to continue down tree
+    if (currentNode.left) stack.push([currentNode.left, maxSoFar]);
+    if (currentNode.right) stack.push([currentNode.right, maxSoFar]);
+  }
+
+  return goodNodesCount;
+};
