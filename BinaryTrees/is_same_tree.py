4b07981641669fe4de3e6894d51d14bb60444f86
Dalton Pettus <daltpettus@gmail.com>
2026-02-23 17:33:09 -0800

    refactor

--- BinaryTrees/is_same_tree.py
+++ BinaryTrees/is_same_tree.py
@@ -9,7 +9,7 @@ class IsSameTree:
         if not p and not q:
             return True
 
-        if p and not q or q and not p:
+        if not p or not q:
             return False
 
         if p.val != q.val:
