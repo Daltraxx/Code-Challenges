24e37a5ad5c5d30f651eddbd65efbf8addbb8d07
Dalton Pettus <daltpettus@gmail.com>
2026-02-24 14:07:15 -0800

    rename files

--- BinaryTrees/MaxAncestorDiff.java
+++ BinaryTrees/MaxAncestorDiff.java
@@ -9,7 +9,7 @@ A node a is an ancestor of b if either: any child of a is equal to b or any chil
 - The number of nodes in the tree is in the range [2, 5000].
 - node.val is a positive number*/
 
-public class MaxDifferenceBetweenNodeAndAncestor {
+public class MaxAncestorDiff {
     public int maxAncestorDiff(TreeNode root) {
         return dfs(root, root.val, root.val);
     }
