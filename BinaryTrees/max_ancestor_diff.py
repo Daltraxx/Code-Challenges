c41fafa8aa9d5fc8f939d29969e52d82015f80cc
Dalton Pettus <daltpettus@gmail.com>
2026-02-24 14:05:28 -0800

    refactor

--- BinaryTrees/max_ancestor_diff.py
+++ BinaryTrees/max_ancestor_diff.py
@@ -5,25 +5,27 @@ from tree_node import TreeNode
 
 class MaxAncestorDiff:
     def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: TreeNode, max_val: int, min_val: int) -> None:
+        if not root:
+            return 0
+
+        def dfs(node: TreeNode, max_val: int, min_val: int) -> int:
             if not node:
-                return 0
+                return max_val - min_val
 
             new_max_val = max(node.val, max_val)
             new_min_val = min(node.val, min_val)
 
-            max_difference = new_max_val - new_min_val
-
             max_left = dfs(node.left, new_max_val, new_min_val)
             max_right = dfs(node.right, new_max_val, new_min_val)
 
-            return max(max_difference, max_left, max_right)
+            return max(max_left, max_right)
 
-        return dfs(root, float("-inf"), float("inf"))
+        return dfs(root, root.val, root.val)
+
 
 # Time Complexity: O(n) where n is the number of nodes in the tree.
 # Space Complexity: O(h) where h is the height of the tree.
-# In the worst case of a skewed tree, the height could be O(n), 
+# In the worst case of a skewed tree, the height could be O(n),
 # leading to O(n) space complexity due to the recursive call stack.
-# In the best case of a balanced tree, the height would be O(log n), 
+# In the best case of a balanced tree, the height would be O(log n),
 # leading to O(log n) space complexity.
