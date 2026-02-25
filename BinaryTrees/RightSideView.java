246a5444db85a422d27568ba994d0da71f9c10a2
Dalton Pettus <daltpettus@gmail.com>
2026-02-25 15:55:10 -0800

    refactor java solution

--- BinaryTrees/RightSideView.java
+++ BinaryTrees/RightSideView.java
@@ -1,9 +1,9 @@
 package BinaryTrees;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.Deque;
 import java.util.List;
-import java.util.Queue;
 
 /*Given the root of a binary tree, 
 imagine yourself standing on the right side of it, 
@@ -11,30 +11,34 @@ return the values of the nodes you can see ordered from top to bottom. */
 
 public class RightSideView {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> viewableNodes = new ArrayList<>();
+        List<Integer> viewableNodeVals = new ArrayList<>();
 
         if (root == null) {
-            return viewableNodes;
+            return viewableNodeVals;
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
             int currentLevelLength = queue.size();
+            viewableNodeVals.add(queue.getLast().val);
             for (int i = 0; i < currentLevelLength; i++) {
-                TreeNode node = queue.remove();
-
-                //if last node in the current level, it is viewable
-                if (i == currentLevelLength - 1) {
-                    viewableNodes.add(node.val);
-                }
-
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+                TreeNode node = queue.removeFirst();
+                if (node.left != null)
+                    queue.addLast(node.left);
+                if (node.right != null)
+                    queue.addLast(node.right);
             }
         }
 
-        return viewableNodes;
+        return viewableNodeVals;
     }
 }
+
+// Time Complexity: O(n) where n is the number of nodes in the tree, as we visit
+// each node once.
+// Space Complexity: O(w) where w is the maximum width of the tree,
+// which occurs when the tree is a complete binary tree.
+// In the worst case, the width can be O(n/2) = O(n). In the best case of a
+// skewed tree, the width would be O(1).
