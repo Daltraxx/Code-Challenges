82a5ce39dcb90f2ebcbda23a36ed4df4e1533c43
Dalton Pettus <daltpettus@gmail.com>
2026-02-27 07:40:08 -0800

    minimum absolute difference in bst python solution

---
+++ BinarySearchTrees/get_minimum_difference.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from BinaryTrees.tree_node import TreeNode
+
+
+class GetMinimumDifference:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        min_difference = float("inf")
+        prev_val = None
+
+        def dfs(node: TreeNode) -> None:
+            if not node:
+                return
+
+            dfs(node.left)
+            nonlocal min_difference, prev_val
+            if prev_val != None:
+                min_difference = min(abs(node.val - prev_val), min_difference)
+            prev_val = node.val
+            dfs(node.right)
+
+        dfs(root)
+        return min_difference
+
+# Time Complexity: O(n) where n is the number of nodes in the tree, as we visit each node once.
+# Space Complexity: O(h) where h is the height of the tree, which in the
+# worst case (a skewed tree) is O(n) and in the best case (a balanced tree) is O(log n).
