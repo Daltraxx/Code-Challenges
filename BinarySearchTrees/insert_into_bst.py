e223fa5996b41efbd20e3948118fb713819f8e1b
Dalton Pettus <daltpettus@gmail.com>
2026-02-28 13:41:51 -0800

    add iterative solution

--- BinarySearchTrees/insert_into_bst.py
+++ BinarySearchTrees/insert_into_bst.py
@@ -15,7 +15,26 @@ class InsertIntoBST:
 
         return root
 
-# Time Complexity: O(h) where h is the height of the tree. 
-# In the worst case of a skewed tree, the height could be O(n), 
-# or O(log n) in the best case of a balanced tree.
-# Space Complexity: O(h) due to the recursive call stack.
+    # Time Complexity: O(h) where h is the height of the tree.
+    # In the worst case of a skewed tree, the height could be O(n),
+    # or O(log n) in the best case of a balanced tree.
+    # Space Complexity: O(h) due to the recursive call stack.
+
+    def insertIntoBSTIterative(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        dummy = root
+        while True:
+            if val < dummy.val:
+                if dummy.left:
+                    dummy = dummy.left
+                else:
+                    dummy.left = TreeNode(val)
+                    return root
+            else:
+                if dummy.right:
+                    dummy = dummy.right
+                else:
+                    dummy.right = TreeNode(val)
+                    return root
