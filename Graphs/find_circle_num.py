6187d0f607f8d2617b49631c482fe01fa01278cb
Dalton Pettus <daltpettus@gmail.com>
2026-03-01 13:16:56 -0800

    refactor

--- Graphs/find_circle_num.py
+++ Graphs/find_circle_num.py
@@ -6,11 +6,9 @@ class FindCircleNum:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
 
         def dfs(city: int):
-            nonlocal cities
-            nonlocal seen
+            seen.add(city)
             for neighbor in cities[city]:
                 if neighbor not in seen:
-                    seen.add(neighbor)
                     dfs(neighbor)
 
         n = len(isConnected)
@@ -18,16 +16,16 @@ class FindCircleNum:
         # Pre-process graph
         cities = defaultdict(list)
         for i in range(n):
-            for j in range(n):
+            for j in range(i + 1,n):
                 if isConnected[i][j] == 1:
                     cities[i].append(j)
+                    cities[j].append(i)
 
         seen = set()
         province_count = 0
         for city in range(n):
             if city not in seen:
                 province_count += 1
-                seen.add(city)
                 dfs(city)
 
         return province_count
