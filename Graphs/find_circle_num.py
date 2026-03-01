0053eb120a2ec4b2c4b21bbb36db6aed8a9dac8b
Dalton Pettus <daltpettus@gmail.com>
2026-03-01 13:37:53 -0800

    add solution without pre-processing

--- Graphs/find_circle_num.py
+++ Graphs/find_circle_num.py
@@ -4,7 +4,6 @@ from typing import List
 
 class FindCircleNum:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-
         def dfs(city: int):
             seen.add(city)
             for neighbor in cities[city]:
@@ -16,7 +15,7 @@ class FindCircleNum:
         # Pre-process graph
         cities = defaultdict(list)
         for i in range(n):
-            for j in range(i + 1,n):
+            for j in range(i + 1, n):
                 if isConnected[i][j] == 1:
                     cities[i].append(j)
                     cities[j].append(i)
@@ -29,10 +28,28 @@ class FindCircleNum:
                 dfs(city)
 
         return province_count
-    
-    # Time Complexity: O(n^2) where n is the number of cities, 
-    # as we need to iterate through the adjacency matrix to build the graph and in the worst case, 
+
+    # Time Complexity: O(n^2) where n is the number of cities,
+    # as we need to iterate through the adjacency matrix to build the graph and in the worst case,
     # we may visit all cities during DFS.
-    # Space Complexity: O(n + e) where n is the number of cities and e is the number of connections between cities, 
-    # as we need to store the graph and the seen set. 
+    # Space Complexity: O(n + e) where n is the number of cities and e is the number of connections between cities,
+    # as we need to store the graph and the seen set.
     # In the worst case, e can be O(n^2) if all cities are connected to each other.
+
+    def findCircleNumNoPreProcess(self, isConnected: List[List[int]]) -> int:
+        def dfs(city: int):
+            seen.add(city)
+            potential_neighbors = len(isConnected[city])
+            for neighbor in range(potential_neighbors):
+                if isConnected[city][neighbor] == 1 and neighbor not in seen:
+                    dfs(neighbor)
+
+        n = len(isConnected)
+        province_count = 0
+        seen = set()
+        for city in range(n):
+            if city not in seen:
+                province_count += 1
+                dfs(city)
+        
+        return province_count
