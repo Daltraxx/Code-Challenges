f42dd4ca8af8a2f02944425660ad33374a0122b9
Dalton Pettus <daltpettus@gmail.com>
2026-03-02 11:15:24 -0800

    refactor java solution

--- Graphs/FindCircleNum.java
+++ Graphs/FindCircleNum.java
@@ -1,8 +1,9 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 
 /*There are n cities. Some of them are connected, while some are not. 
 If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
@@ -22,29 +23,31 @@ public class FindCircleNum {
     boolean[] seen;
 
     public int findCircleNum(int[][] isConnected) {
-        //build undirected graph with given adjacency matrix
+        // Build undirected graph with given adjacency matrix
         int n = isConnected.length;
         for (int i = 0; i < n; i++) {
-            if (!graph.containsKey(i)) graph.put(i, new ArrayList<>());
-            
+            if (!graph.containsKey(i))
+                graph.put(i, new ArrayList<>());
+
             for (int j = i + 1; j < n; j++) {
-                if (!graph.containsKey(j)) graph.put(j, new ArrayList<>());
-                
+                if (!graph.containsKey(j))
+                    graph.put(j, new ArrayList<>());
+
                 if (isConnected[i][j] == 1) {
                     graph.get(i).add(j);
                     graph.get(j).add(i);
                 }
             }
-        } //graph built
+        }
 
         seen = new boolean[n];
         int provincesCount = 0;
 
         for (int node = 0; node < n; node++) {
-            //if a node has not been seen, it's an unvisited province
+            // If a node has not been seen, it's an unvisited province
             if (!seen[node]) {
-                //increment provinces and
-                //set all nodes of connected component to true in seen
+                // Increment provinces and
+                // set all nodes of connected component to true in seen
                 provincesCount++;
                 seen[node] = true;
                 dfs(node);
@@ -54,9 +57,10 @@ public class FindCircleNum {
         return provincesCount;
     }
 
+    // RECURSIVE DFS
     public void dfs(int node) {
         for (int neighbor : graph.get(node)) {
-            //add all neighbors to seen, completing component (provence)
+            // Add all neighbors to seen, completing component (province)
             if (!seen[neighbor]) {
                 seen[neighbor] = true;
                 dfs(neighbor);
@@ -64,20 +68,29 @@ public class FindCircleNum {
         }
     }
 
-    //iterative dfs example
+    // ITERATIVE DFS
     public void dfsIterative(int node) {
-        Stack<Integer> stack = new Stack<>();
-        stack.add(node);
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addLast(node);
 
         while (!stack.isEmpty()) {
-            int currentNode = stack.pop();
+            int currentNode = stack.removeLast();
 
             for (int neighbor : graph.get(currentNode)) {
                 if (!seen[neighbor]) {
                     seen[neighbor] = true;
-                    stack.add(neighbor);
+                    stack.addLast(neighbor);
                 }
             }
         }
     }
 }
+
+// Time Complexity: O(n^2) where n is the number of cities,
+// as we need to iterate through the adjacency matrix to build the graph and in
+// the worst case,
+// we may visit all cities during DFS.
+// Space Complexity: O(n + e) where n is the number of cities and e is the
+// number of connections between cities,
+// as we need to store the graph and the seen set.
+// In the worst case, e can be O(n^2) if all cities are connected to each other.
