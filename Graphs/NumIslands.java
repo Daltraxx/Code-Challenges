1b9c9751b2d2fad26bd9bddf2a35bc19797ed2ec
Dalton Pettus <daltpettus@gmail.com>
2026-03-02 12:47:24 -0800

    rename files

--- Graphs/NumIslands.java
+++ Graphs/NumIslands.java
@@ -6,7 +6,7 @@ return the number of islands.
 An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. 
 You may assume all four edges of the grid are all surrounded by water.*/
 
-public class NumberOfIslands {
+public class NumIslands {
     int height;
     int width;
     boolean[][] seen;
