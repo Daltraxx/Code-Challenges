c99d7bafa07a620fa783410d5d59ddfb895fd940
Dalton Pettus <daltpettus@gmail.com>
2026-03-03 11:43:44 -0800

    use tuples instead of hashing string

--- Graphs/min_reorder.py
+++ Graphs/min_reorder.py
@@ -4,14 +4,11 @@ from typing import List
 
 class MinReorder:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        def hash_route(origin: int, dest: int) -> str:
-            return f"{origin}-{dest}"
-
         def dfs(city: int):
             seen[city] = True
             for neighbor in edges[city]:
                 if not seen[neighbor]:
-                    if hash_route(city, neighbor) in original_routes:
+                    if (city, neighbor) in original_routes:
                         nonlocal flip_count
                         flip_count += 1
                     dfs(neighbor)
@@ -20,7 +17,7 @@ class MinReorder:
         edges = defaultdict(list)
         original_routes = set()
         for origin, dest in connections:
-            original_routes.add(hash_route(origin, dest))
+            original_routes.add((origin, dest))
             edges[origin].append(dest)
             edges[dest].append(origin)
 
@@ -31,7 +28,7 @@ class MinReorder:
         flip_count = 0
         for neighbor in edges[0]:
             if not seen[neighbor]:
-                if hash_route(0, neighbor) in original_routes:
+                if (0, neighbor) in original_routes:
                     flip_count += 1
                 dfs(neighbor)
         
