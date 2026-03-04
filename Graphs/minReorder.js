96b07a1c33e44eb9955faa527e0ef4ee0d971ae7
Dalton Pettus <daltpettus@gmail.com>
2026-03-03 16:36:52 -0800

    refactor js solution

--- Graphs/minReorder.js
+++ Graphs/minReorder.js
@@ -9,47 +9,47 @@ Your task consists of reorienting some roads such that each city can visit the c
 
 It's guaranteed that each city can reach city 0 after reorder.*/
 
-const convertToHash = (start, end) => {
-    return `${start}, ${end}`;
-}
-
 const minReorder = (n, connections) => {
-    const graph = new Map();
-    const routes = new Set();
+  const graph = new Map();
 
-    //build graph and set of original routes
-    for (let [start, end] of connections) {
-        routes.add(convertToHash(start, end));
+  // Build graph and with each edge store flag if part of original directed edge
+  for (let [start, end] of connections) {
+    if (!graph.has(start)) graph.set(start, []);
+    if (!graph.has(end)) graph.set(end, []);
 
-        if (!graph.has(start)) graph.set(start, []);
-        if (!graph.has(end)) graph.set(end, []);
+    graph.get(start).push([end, true]);
+    graph.get(end).push([start, false]);
+  }
 
-        graph.get(start).push(end);
-        graph.get(end).push(start);
-    }
+  const seen = new Array(n).fill(false);
 
-    const seen = new Array(n).fill(false);
-
-    const dfs = (startingPoint) => {
-        let routeFlips = 0;
-        for (let destination of graph.get(startingPoint)) {
-            if (!seen[destination]) {
-                seen[destination] = true;
-
-                if (routes.has(convertToHash(startingPoint, destination))) {
-                    routeFlips++;
-                }
-
-                routeFlips += dfs(destination);
-            }
+  const dfs = (startingPoint) => {
+    seen[startingPoint] = true;
+    let routeFlips = 0;
+    for (let [destination, isOriginalRoute] of graph.get(startingPoint)) {
+      if (!seen[destination]) {
+        if (isOriginalRoute) {
+          routeFlips++;
         }
-
-        return routeFlips;
+        routeFlips += dfs(destination);
+      }
     }
 
-    seen[0] = true;
-    return dfs(0);
-}
+    return routeFlips;
+  };
 
-const n = 5, connections = [[1,0],[1,2],[3,2],[3,4]];
+  return dfs(0);
+};
+
+// Time complexity: O(n) where n is the number of cities. 
+// We visit each city once and each edge once (we're guaranteed to have n - 1 edges).
+// Space complexity: O(n) for the graph and the seen array.
+
+const n = 5,
+  connections = [
+    [1, 0],
+    [1, 2],
+    [3, 2],
+    [3, 4],
+  ];
 console.log(minReorder(n, connections));
