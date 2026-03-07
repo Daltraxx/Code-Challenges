d57307786a1e807cafc52eaa5bf2cc51b8a90cf4
Dalton Pettus <daltpettus@gmail.com>
2026-03-07 10:24:37 -0800

    refactor js solution

--- Graphs/reachableNodesWithRestrictions.js
+++ Graphs/reachableNodesWithRestrictions.js
@@ -8,35 +8,35 @@ Return the maximum number of nodes you can reach from node 0 without visiting a 
 Note that node 0 will not be a restricted node.*/
 
 const reachableNodes = (n, edges, restricted) => {
-    const getReachableNodesCount = (node) => {
-        let count = 1;
+  const dfs = (node) => {
+    let count = 1;
 
-        for (let neighbor of adjacencyList[node]) {
-            if (!seen[neighbor]) {
-                seen[neighbor] = true;
-                count += getReachableNodesCount(neighbor);
-            }
-        }
-
-        return count;
+    for (let neighbor of adjacencyList[node]) {
+      if (!seen[neighbor]) {
+        seen[neighbor] = true;
+        count += dfs(neighbor);
+      }
     }
 
-    //build adjacency list
-    const adjacencyList = new Array(n);
-    for (let node = 0; node < n; node++) {
-        adjacencyList[node] = [];
-    }
+    return count;
+  };
 
-    for (let [nodeA, nodeB] of edges) {
-        adjacencyList[nodeA].push(nodeB);
-        adjacencyList[nodeB].push(nodeA);
-    }
+  // Build adjacency list
+  const adjacencyList = Array.from({ length: n }, () => []);
 
-    const seen = new Array(n).fill(false);
-    for (let node of restricted) {
-        seen[node] = true;
-    }
-    seen[0] = true;
+  for (let [nodeA, nodeB] of edges) {
+    adjacencyList[nodeA].push(nodeB);
+    adjacencyList[nodeB].push(nodeA);
+  }
 
-    return getReachableNodesCount(0);
-}
+  const seen = new Array(n).fill(false);
+  for (let node of restricted) {
+    seen[node] = true;
+  }
+  seen[0] = true;
+
+  return dfs(0);
+};
+
+// Time complexity: O(n) - We visit each node at most once.
+// Space complexity: O(n) - The adjacency list and the seen array both take O(n) space.
