0cf222c91369e89bbfc3d12b7e4a3bc97e4209c8
Dalton Pettus <daltpettus@gmail.com>
2026-03-07 11:56:50 -0800

    refactor

--- Graphs/shortest_path_binary_matrix.py
+++ Graphs/shortest_path_binary_matrix.py
@@ -17,21 +17,20 @@ class ShortestPathBinaryMatrix:
             (-1, -1),
             (-1, 1),
         )
-        height = len(grid)
-        width = len(grid[0])
+        n = len(grid)
 
         def is_valid(row, col):
             return (
-                0 <= row < height
-                and 0 <= col < width
+                0 <= row < n
+                and 0 <= col < n
                 and not seen[row][col]
                 and grid[row][col] == 0
             )
 
         def is_end(row, col):
-            return row == height - 1 and col == width - 1 and grid[row][col] == 0
+            return row == n - 1 and col == n - 1 and grid[row][col] == 0
 
-        seen = [[False for _ in range(width)] for _ in range(height)]
+        seen = [[False] * n for _ in range(n)]
         seen[0][0] = True
         steps = 1
         queue = deque([(0, 0)])
@@ -54,3 +53,6 @@ class ShortestPathBinaryMatrix:
 
         return -1
 
+# Time Complexity: O(n^2) where n is the number of rows and columns in the grid,
+# as we may need to visit each cell in the grid at most once during BFS.
+# Space Complexity: O(n^2) for the `seen` matrix and the queue in the worst case.
