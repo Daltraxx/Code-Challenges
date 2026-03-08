51be269ba480b9926a587a367494a0c9a6860340
Dalton Pettus <daltpettus@gmail.com>
2026-03-08 11:01:54 -0700

    refactor js solution

--- Graphs/shortestPathBinaryMatrix.js.js
+++ Graphs/shortestPathBinaryMatrix.js.js
@@ -7,53 +7,70 @@ A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) 
 The length of a clear path is the number of visited cells of this path. */
 
 const shortestPathBinaryMatrix = (grid) => {
-    const getShortestPathCount = (startingRow, startingColumn) => {
-        if (grid[startingRow][startingColumn] === 1 || grid[n - 1][n - 1] === 1) {
-            return -1;
+  const getShortestPathCount = (startingRow, startingColumn) => {
+    if (grid[startingRow][startingColumn] === 1 || grid[n - 1][n - 1] === 1) {
+      return -1;
+    }
+
+    let queue = [[startingRow, startingColumn]];
+    let pathLength = 0;
+
+    while (queue.length) {
+      pathLength++;
+      const nextLevelQueue = [];
+
+      for (let position = 0; position < queue.length; position++) {
+        const [currentRow, currentCol] = queue[position];
+
+        if (currentRow === n - 1 && currentCol === n - 1) {
+          return pathLength;
         }
 
-        let queue = [[startingRow, startingColumn]];
-        let pathLength = 0;
+        for (let [directionX, directionY] of directions) {
+          const nextRow = currentRow + directionY;
+          const nextCol = currentCol + directionX;
 
-        while (queue.length) {
-            pathLength++;
-            const nextLevelQueue = [];
-
-            for (let position = 0; position < queue.length; position++) {
-                const [currentRow, currentCol] = queue[position];
-
-                if (currentRow === n - 1 && currentCol === n - 1) {
-                    return pathLength;
-                }
-
-                for (let [directionX, directionY] of directions) {
-                    const nextRow = currentRow + directionY;
-                    const nextCol = currentCol + directionX;
-
-                    if (isValid(nextRow, nextCol)) {
-                        seen[nextRow][nextCol] = true;
-                        nextLevelQueue.push([nextRow, nextCol]);
-                    }
-
-                }
-            }
-            queue = nextLevelQueue;
+          if (isValid(nextRow, nextCol)) {
+            seen[nextRow][nextCol] = true;
+            nextLevelQueue.push([nextRow, nextCol]);
+          }
         }
-
-        return -1;
+      }
+      queue = nextLevelQueue;
     }
 
-    const isValid = (row, col) => {
-        return row >= 0 && row < n && col >= 0 && col < n && !seen[row][col] && grid[row][col] === 0;
-    }
+    return -1;
+  };
 
-    const directions = [[-1, 0], [1, 0] , [0, -1,], [0, 1], [-1, -1], [1, -1], [-1, 1], [1, 1]];
-    const n = grid.length;
-    const seen = new Array(n);
-    for (let row = 0; row < n; row++) {
-        seen[row] = new Array(n).fill(false);
-    }
+  const isValid = (row, col) => {
+    return (
+      row >= 0 &&
+      row < n &&
+      col >= 0 &&
+      col < n &&
+      !seen[row][col] &&
+      grid[row][col] === 0
+    );
+  };
 
-    seen[0][0] = true;
-    return getShortestPathCount(0, 0);
-}
+  const directions = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+    [-1, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+  ];
+  const n = grid.length;
+  const seen = Array.from({ length: n }, () => Array(n).fill(false));
+
+  seen[0][0] = true;
+  return getShortestPathCount(0, 0);
+};
+
+// Time Complexity: O(n^2) where n is the length of the grid. In the worst case,
+// we may have to visit all cells in the grid.
+// Space Complexity: O(n^2) in the worst case, we may have to store all cells in
+// the queue and the seen array.
