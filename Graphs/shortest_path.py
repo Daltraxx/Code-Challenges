a8456b982f56765b951546bea9bddcfdefc0036e
Dalton Pettus <daltpettus@gmail.com>
2026-03-09 08:27:48 -0700

    fix boundary bug

--- Graphs/shortest_path.py
+++ Graphs/shortest_path.py
@@ -17,14 +17,10 @@ class ShortestPath:
             for dy, dx in directions:
                 new_row = row + dy
                 new_col = col + dx
-                if 0 <= row < height and 0 <= col < width:
-                    new_remaining = remaining if grid[new_row][new_col] is not 1 else remaining - 1
+                if 0 <= new_row < height and 0 <= new_col < width:
+                    new_remaining = remaining if grid[new_row][new_col] != 1 else remaining - 1
                     if new_remaining > seen[new_row][new_col]:
                         seen[new_row][new_col] = new_remaining
                         queue.append((new_row, new_col, steps + 1, new_remaining))
 
         return -1
-
-# Time Complexity: O(m * n * k) where m and n are the dimensions of the grid,
-# and k is the number of obstacles we can eliminate, as we may need to visit each cell with different remaining eliminations.
-# Space Complexity: O(m * n * k) for the seen list (O(m * n)) and the queue in the worst case.
