04753335da0a6da9a3030c085915250aa83a14ac
Dalton Pettus <daltpettus@gmail.com>
2026-03-11 10:33:04 -0700

    refactor python solution

--- Graphs/shortest_alternating_paths.py
+++ Graphs/shortest_alternating_paths.py
@@ -21,29 +21,33 @@ class ShortestAlternatingPaths:
         seen = [[False, False] for _ in range(n)]
         seen[0] = [True, True]
         distances = [-1] * n
+        RED = 0
+        BLUE = 1
 
         # Queue stores tuples of the form (node, steps, prev_color)
-        queue = deque([(0, 0, None)])
+        queue = deque([(0, 0, RED), (0, 0, BLUE)])
         while queue:
             node, steps, prev_edge = queue.popleft()
             if distances[node] == -1:
                 distances[node] = steps
 
-            if not prev_edge or prev_edge == "red":
+            if prev_edge == RED:
                 # Traverse blue edges
                 for neighbor in blue_graph[node]:
-                    if not seen[neighbor][1]:
-                        seen[neighbor][1] = True
-                        queue.append((neighbor, steps + 1, "blue"))
-            if not prev_edge or prev_edge == "blue":
+                    if not seen[neighbor][BLUE]:
+                        seen[neighbor][BLUE] = True
+                        queue.append((neighbor, steps + 1, BLUE))
+            if prev_edge == BLUE:
                 # Traverse red edges
                 for neighbor in red_graph[node]:
-                    if not seen[neighbor][0]:
-                        seen[neighbor][0] = True
-                        queue.append((neighbor, steps + 1, "red"))
+                    if not seen[neighbor][RED]:
+                        seen[neighbor][RED] = True
+                        queue.append((neighbor, steps + 1, RED))
 
         return distances
 
-# Time Complexity: O(n + e) where n is the number of nodes and e is the total number of edges,
-# since the range of color is always 2 (constant), and we may need to visit each node and edge at most once.
-# Space Complexity: O(n + e) for the graph and seen array in the worst case.
+
+# Time Complexity: O(n + r + b) where n is the number of nodes,
+# r is the number of red edges,
+# and b is the number of blue edges.
+# Space Complexity: O(n + r + b) for the graph representation and the seen array
