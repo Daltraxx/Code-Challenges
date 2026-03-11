c707f4b1d24aa7fc9090684988077000d98c25d1
Dalton Pettus <daltpettus@gmail.com>
2026-03-11 11:40:59 -0700

    refactor java solution

--- Graphs/ShortestAlternatingPaths.java
+++ Graphs/ShortestAlternatingPaths.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /*You are given an integer n, the number of nodes in a directed graph where the nodes are labeled from 0 to n - 1. 
@@ -19,73 +18,67 @@ where each answer[x] is the length of the shortest path from node 0 to node x
 such that the edge colors alternate along the path, 
 or -1 if such a path does not exist. */
 
-
-
 public class ShortestAlternatingPaths {
     int n;
-    int RED = 0;
-    int BLUE = 1;
-    Map<Integer, Map<Integer, List<Integer>>> graph = new HashMap<>();
+    private static final int RED = 0;
+    private static final int BLUE = 1;
+    Map<Integer, ArrayList<ArrayList<Integer>>> graph = new HashMap<>();
 
     public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
         this.n = n;
+        graph.put(RED, new ArrayList<>());
+        graph.put(BLUE, new ArrayList<>());
 
-        graph.put(RED, new HashMap<>());
-        graph.put(BLUE, new HashMap<>());
-
-        addToGraph(RED, redEdges);
-        addToGraph(BLUE, blueEdges);
+        generateAdjacencyList(RED, redEdges);
+        generateAdjacencyList(BLUE, blueEdges);
 
         int[] shortestPaths = new int[n];
         Arrays.fill(shortestPaths, -1);
 
-        Deque<State> queue = new ArrayDeque<>();
-        queue.add(new State(0, RED, 0));
-        queue.add(new State(0, BLUE, 0));
+        // Queue stores int arrays of the form [node, prev_color, steps]
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] { 0, RED, 0 });
+        queue.add(new int[] { 0, BLUE, 0 });
 
         boolean[][] seen = new boolean[n][2];
         seen[0][RED] = seen[0][BLUE] = true;
 
         while (!queue.isEmpty()) {
-            State state = queue.remove();
-            int node = state.node, colorRequired = state.colorRequired, steps = state.steps;
+            int[] state = queue.pollFirst();
+            int node = state[0];
+            int prevColor = state[1];
+            int steps = state[2];
 
             if (shortestPaths[node] == -1) {
                 shortestPaths[node] = steps;
             }
 
-            for (int neighbor : graph.get(colorRequired).get(node)) {
-                int nextColorRequired = 1 - colorRequired;
-                if (!seen[neighbor][nextColorRequired]) {
-                    seen[neighbor][nextColorRequired] = true;
-                    queue.add(new State(neighbor, nextColorRequired, steps + 1));
+            int currentColor = 1 - prevColor;
+            for (int neighbor : graph.get(currentColor).get(node)) {
+                if (!seen[neighbor][currentColor]) {
+                    seen[neighbor][currentColor] = true;
+                    queue.addLast(new int[] { neighbor, currentColor, steps + 1 });
                 }
             }
         }
 
-        return shortestPaths; 
+        return shortestPaths;
     }
 
-    public void addToGraph(int color, int[][] edges) {
+    public void generateAdjacencyList(int color, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adjacencyList = graph.get(color);
         for (int node = 0; node < n; node++) {
-            graph.get(color).put(node, new ArrayList<>());
+            adjacencyList.add(new ArrayList<>());
         }
 
         for (int[] edge : edges) {
-            int nodeA = edge[0], nodeB = edge[1];
-            graph.get(color).get(nodeA).add(nodeB);
-        }
-    }
-    
-    private static class State {
-        int node;
-        int colorRequired;
-        int steps;
-
-        public State(int node, int colorRequired, int steps) {
-            this.node = node;
-            this.colorRequired = colorRequired;
-            this.steps = steps;
+            adjacencyList.get(edge[0]).add(edge[1]);
         }
     }
 }
+
+// Time Complexity: O(n + r + b) where n is the number of nodes,
+// r is the number of red edges,
+// and b is the number of blue edges.
+// Space Complexity: O(n + r + b) for the graph representation and the seen
+// array.
