ca5d866d2b7988339da57b112c362c0ef11a5245
Dalton Pettus <daltpettus@gmail.com>
2026-03-15 17:37:39 -0700

    refactor

--- ArrayAndString/can_place_flowers.py
+++ ArrayAndString/can_place_flowers.py
@@ -3,20 +3,23 @@ from typing import List
 
 class CanPlaceFlowers:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        if n == 0:
+            return True
         m = len(flowerbed)
         new_flowerbed = flowerbed.copy()
         count = 0
         for i in range(m):
-            if i == 0:
-                if new_flowerbed[i] == 0 and new_flowerbed[i + 1] == 0:
-                    count += 1
+            if new_flowerbed[i] == 0:
+                left = i == 0 or new_flowerbed[i - 1] == 0
+                right = i == m - 1 or new_flowerbed[i + 1] == 0
+                if left and right:
                     new_flowerbed[i] = 1
-            elif i == m - 1:
-                if new_flowerbed[i - 1] == 0 and new_flowerbed[i] == 0:
                     count += 1
-            else:
-                if new_flowerbed[i - 1] == 0 and new_flowerbed[i] and new_flowerbed[i + 1] == 0:
-                    count += 1
-                    new_flowerbed[i] = 1
+                    if count == n:
+                        return True
 
-        return count >= n
+        return False
+
+# Time Complexity: O(m) where m is the length of the flowerbed array.
+# We iterate through the flowerbed once to check for available spots and place flowers.
+# Space Complexity: O(m) for the new_flowerbed array, which is a copy of the input flowerbed.
