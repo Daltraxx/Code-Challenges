b37650f438f58b0f7cc80dc3ded0810446821a2e
Dalton Pettus <daltpettus@gmail.com>
2026-03-16 10:28:19 -0700

    refactor java solution

--- Graphs/MinMutation.java
+++ Graphs/MinMutation.java
@@ -1,71 +1,63 @@
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-/*A gene string can be represented by an 8-character long string, 
-with choices from 'A', 'C', 'G', and 'T'.
-
-Suppose we need to investigate a mutation from a gene string startGene to a gene string endGene where one mutation is defined as one single character changed in the gene string.
-
-For example, "AACCGGTT" --> "AACCGGTA" is one mutation.
-There is also a gene bank bank that records all the valid gene mutations. 
-A gene must be in bank to make it a valid gene string.
-
-Given the two gene strings startGene and endGene and the gene bank bank, 
-return the minimum number of mutations needed to mutate from startGene to endGene. 
-If there is no such a mutation, return -1.
-
-Note that the starting point is assumed to be valid, so it might not be included in the bank.*/
 
 class MinMutation {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        Set<String> seen = new HashSet<>();
-        seen.add(startGene);
+        HashSet<String> bankSet = new HashSet<>(Arrays.asList(bank));
+        if (!bankSet.contains(endGene)) {
+            return -1;
+        }
 
+        char[] bases = { 'A', 'C', 'G', 'T' };
+
+        // Can use the bankSet to track visited genes as well,
+        // since we remove genes from it once they are visited.
+        if (bankSet.contains(startGene)) {
+            bankSet.remove(startGene);
+        }
         Deque<String> queue = new ArrayDeque<>();
         queue.add(startGene);
-        int mutationCount = 0;
+        int mutations = 0;
 
         while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
+            int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
                 String gene = queue.remove();
 
                 if (gene.equals(endGene)) {
-                    return mutationCount;
+                    return mutations;
                 }
 
-                for (String neighbor : getNeighbors(gene)) {
-                    if (!seen.contains(neighbor) && Arrays.asList(bank).contains(neighbor)) {
-                        seen.add(neighbor);
-                        queue.add(neighbor);
+                for (int j = 0; j < gene.length(); j++) {
+                    for (char base : bases) {
+                        if (gene.charAt(j) == base) {
+                            continue;
+                        }
+                        char[] geneArray = gene.toCharArray();
+                        geneArray[j] = base;
+                        String newGene = new String(geneArray);
+                        if (bankSet.contains(newGene)) {
+                            bankSet.remove(newGene);
+                            queue.add(newGene);
+                        }
                     }
                 }
-
             }
-
-            mutationCount++;
+            mutations++;
         }
 
         return -1;
     }
-    
-    public List<String> getNeighbors(String gene) {
-        char[] bases = {'A', 'C', 'G', 'T'};  
-        List<String> neighbors = new ArrayList<>();
-
-        for (int i = 0; i < gene.length(); i++) {
-            for (char base : bases) {
-                if (base != gene.charAt(i)) {
-                    neighbors.add(gene.substring(0, i) + base + gene.substring(i + 1));
-                }
-            }
-        }
-
-        return neighbors;
-    }
 }
+
+// Time Complexity: O(n) where n is the number of genes in the bank,
+// since we convert the bank array to a set and may have to visit each gene at
+// most once during the BFS.
+// Technically, the BFS runs in constant time since the gene length is fixed at
+// 8 and there are only 4 possible bases.
+// Space Complexity: O(n) for the bankSet and the queue in the worst case where
+// we visit all genes in the bank,
+// though could also be considered O(1) since the gene length is fixed and there
+// are only 4^8 possible genes.
