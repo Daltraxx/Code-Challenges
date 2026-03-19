f16832fce1d8bebc50bd5c0ae650058091491262
Dalton Pettus <daltpettus@gmail.com>
2026-03-19 08:37:12 -0700

    refactor

--- Graphs/ladder_length.py
+++ Graphs/ladder_length.py
@@ -18,17 +18,26 @@ class LadderLength:
                     return count
                 wordChars = list(word)
                 for i in range(n):
-                    char = wordChars[i]
-                    for change in range(1, 27):
-                        char_code = 97 + (ord(char) - 97 + change) % 26
-                        wordChars[i] = chr(char_code)
+                    original_char = wordChars[i]
+                    for new_char in "abcdefghijklmnopqrstuvwxyz":
+                        if new_char == original_char:
+                            continue
+                        wordChars[i] = new_char
                         new_word = "".join(wordChars)
                         if new_word in wordSet:
                             wordSet.remove(new_word)
                             queue.append(new_word)
                         # Reset
-                        wordChars[i] = char
+                        wordChars[i] = original_char
 
             count += 1
 
         return 0
+
+# Time complexity: O(n * m * 26) where n is the number of words in the word list,
+# m is the length of each word, and 26 is the number of possible character changes
+# (since we can change each character to any of the 26 letters).
+# In the worst case, we may have to visit all words in the word list, and
+# for each word, we check all m characters and try changing it to 26 different letters.
+# Space complexity: O(n) for the queue and the word set, where n is the
+# number of words in the word list. In the worst case, we may have to store all words in the queue and the set.
