66998b510dc0a0b0564b396f413764dcbc6bf65f
Dalton Pettus <daltpettus@gmail.com>
2026-03-20 11:51:08 -0700

    refactor

--- Graphs/ladder_length.py
+++ Graphs/ladder_length.py
@@ -5,17 +5,17 @@ from typing import List
 class LadderLength:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         n = len(beginWord)
-        wordSet = set(wordList)
-        if endWord not in wordSet:
+        allowed_words = set(wordList)
+        if endWord not in allowed_words:
             return 0
-        wordSet.add(beginWord)
+        allowed_words.add(beginWord)
         queue = deque([beginWord])
-        count = 1
+        length = 1
         while queue:
             for _ in range(len(queue)):
                 word = queue.popleft()
                 if word == endWord:
-                    return count
+                    return length
                 wordChars = list(word)
                 for i in range(n):
                     original_char = wordChars[i]
@@ -24,13 +24,13 @@ class LadderLength:
                             continue
                         wordChars[i] = new_char
                         new_word = "".join(wordChars)
-                        if new_word in wordSet:
-                            wordSet.remove(new_word)
+                        if new_word in allowed_words:
+                            allowed_words.remove(new_word)
                             queue.append(new_word)
-                        # Reset
+                        # Reset the character back to the original for the next iteration
                         wordChars[i] = original_char
 
-            count += 1
+            length += 1
 
         return 0
 
@@ -50,13 +50,14 @@ class LadderLength:
         if endWord not in allowed_words:
             return 0
         curr_set = {beginWord}
-        next_set = {endWord}
-        length = 1
+        other_set = {endWord}
+        length = 2
         letters = "abcdefghijklmnopqrstuvwxyz"
 
-        while curr_set and next_set:
-            if len(curr_set) > len(next_set):
-                curr_set, next_set = next_set, curr_set
+        while curr_set and other_set:
+            # Always expand the smaller set to optimize the search and minimize branching.
+            if len(curr_set) > len(other_set):
+                curr_set, other_set = other_set, curr_set
 
             next_level = set()
             for word in curr_set:
@@ -69,15 +70,17 @@ class LadderLength:
                         chars[i] = letter
                         new_word = "".join(chars)
 
-                        if new_word in next_set:
-                            return length + 1
+                        # If the new word is in the opposite set, we have found a connection 
+                        # and can return the current length of the transformation sequence.
+                        if new_word in other_set:
+                            return length
 
                         if new_word in allowed_words:
                             allowed_words.remove(new_word)
                             next_level.add(new_word)
 
-                        # Reset word
-                        chars[i] = original
+                    # Reset the character back to the original for the next iteration
+                    chars[i] = original
 
             curr_set = next_level
             length += 1
@@ -85,8 +88,7 @@ class LadderLength:
         return 0
     
     # Time complexity: O(n * m * 26) where n is the number of words in the word list,
-    # m is the length of each word, and 26 is the number of possible character
-    # changes (since we can change each character to any of the 26 letters).
+    # m is the length of each word, and 26 is the number of possible character changes.
     # In the worst case, we may have to visit all words in the word list,
     # and for each word, we check all m characters and try changing it to 26 different letters.
     # In practice, the bidirectional BFS can significantly reduce the number of words we need to visit, 
