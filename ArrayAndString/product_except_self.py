ff9974272cacb2e5089e1a8048efb5f80e85e66f
Dalton Pettus <daltpettus@gmail.com>
2026-03-22 14:32:42 -0700

    refactor

--- ArrayAndString/product_except_self.py
+++ ArrayAndString/product_except_self.py
@@ -4,15 +4,14 @@ from typing import List
 class ProductExceptSelf:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        ans = [1]
-        left_prod = 1
+        ans = [1] * n
         for i in range(1, n):
-            left_prod = ans[i - 1] * nums[i - 1]
-            ans.append(left_prod)
+            # Compute the product of all elements to the left of the current index
+            ans[i] = ans[i - 1] * nums[i - 1]
 
         right_prod = 1
         for i in range(n - 1, -1, -1):
-            ans[i] = ans[i] * right_prod
+            ans[i] *= right_prod
             right_prod *= nums[i]
 
         return ans
