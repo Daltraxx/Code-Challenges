64cc9f652dbb4a4a4345866a09fa4b617ecc441a
Dalton Pettus <daltpettus@gmail.com>
2026-03-24 12:45:01 -0700

    refactor with helper

--- SlidingWindow/compress.py
+++ SlidingWindow/compress.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Compress:
     def compress(self, chars: List[str]) -> int:
+        def write_compressed_char(char: str, char_count: int) -> None:
+            nonlocal write
+
+            if char_count == 1:
+                chars[write] = char
+                write += 1
+                return
+
+            compressed_char = f"{char}{char_count}"
+            for c in compressed_char:
+                chars[write] = c
+                write += 1
+
         n = len(chars)
         write = 0
         left = 0
@@ -10,32 +23,19 @@ class Compress:
             if chars[right] != chars[left]:
                 char = chars[left]
                 char_count = right - left
-                if char_count == 1:
-                    chars[write] = char
-                    write += 1
-                else:
-                    compressed_char = f"{char}{char_count}"
-                    for c in compressed_char:
-                        chars[write] = c
-                        write += 1
+                write_compressed_char(char, char_count)
                 left = right
 
-        if left == n - 1:
-            chars[write] = chars[left]
-            write += 1
-        else:
-            char = chars[left]
-            char_count = n - left
-            compressed_char = f"{char}{char_count}"
-            for c in compressed_char:
-                chars[write] = c
-                write += 1
-        
+        char = chars[left]
+        char_count = n - left
+        write_compressed_char(char, char_count)
+
         return write
 
-# Time complexity: O(n). We iterate through the input list of characters once, 
+
+# Time complexity: O(n). We iterate through the input list of characters once,
 # resulting in O(n) time complexity.
-# Space complexity: O(1). 
-# We use a constant amount of extra space to store the compressed characters, 
-# and we modify the input list in place 
+# Space complexity: O(1).
+# We use a constant amount of extra space to store the compressed characters,
+# and we modify the input list in place
 # without using any additional data structures that grow with the input size.
