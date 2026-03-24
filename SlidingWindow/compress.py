d2d6cf8f30eb1012500754eca765ecc4804f3e50
Dalton Pettus <daltpettus@gmail.com>
2026-03-24 12:29:34 -0700

    compressed string python solution

---
+++ SlidingWindow/compress.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Compress:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        write = 0
+        left = 0
+        for right in range(n):
+            if chars[right] != chars[left]:
+                char = chars[left]
+                char_count = right - left
+                if char_count == 1:
+                    chars[write] = char
+                    write += 1
+                else:
+                    compressed_char = f"{char}{char_count}"
+                    for c in compressed_char:
+                        chars[write] = c
+                        write += 1
+                left = right
+
+        if left == n - 1:
+            chars[write] = chars[left]
+            write += 1
+        else:
+            char = chars[left]
+            char_count = n - left
+            compressed_char = f"{char}{char_count}"
+            for c in compressed_char:
+                chars[write] = c
+                write += 1
+        
+        return write
+
