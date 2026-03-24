dd76e3086ae9e1d6ec2a54dc16595bf411c2ea7d
Dalton Pettus <daltpettus@gmail.com>
2026-03-24 10:15:20 -0700

    clean up java solution

--- Heaps/MinStoneSum.java
+++ Heaps/MinStoneSum.java
@@ -3,7 +3,7 @@ package Heaps;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class RemoveStonesToMinimizeTotal {
+public class MinStoneSum {
     public int minStoneSum(int[] piles, int k) {
         int total = 0;
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
@@ -18,7 +18,16 @@ public class RemoveStonesToMinimizeTotal {
             total -= stonesRemoved;
             maxHeap.add(maxPile - stonesRemoved);
         }
-        
+
         return total;
     }
 }
+
+// Time complexity: O(n + k log n).
+// We build a max heap from the input list of piles, which takes O(n) time.
+// Then, we perform k iterations where we pop the maximum element from the heap
+// and push the reduced pile back into the heap.
+// Each pop and push operation takes O(log n) time, resulting in O(k log n) for
+// k iterations.
+// Space complexity: O(n). We use a max heap to store the piles, which requires
+// O(n) space in the worst case.
