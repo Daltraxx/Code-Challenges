948eb491accc4d32e442cd8aa87242311d2614ac
Dalton Pettus <daltpettus@gmail.com>
2026-03-24 12:54:40 -0700

    refactor

--- SlidingWindow/compress.py
+++ SlidingWindow/compress.py
@@ -3,32 +3,21 @@ from typing import List
 
 class Compress:
     def compress(self, chars: List[str]) -> int:
-        def write_compressed_char(char: str, char_count: int) -> None:
-            nonlocal write
-
-            if char_count == 1:
-                chars[write] = char
-                write += 1
-                return
-
-            compressed_char = f"{char}{char_count}"
-            for c in compressed_char:
-                chars[write] = c
-                write += 1
-
         n = len(chars)
         write = 0
         left = 0
-        for right in range(n):
-            if chars[right] != chars[left]:
+        for right in range(1, n + 1):
+            if right == n or chars[right] != chars[left]:
                 char = chars[left]
                 char_count = right - left
-                write_compressed_char(char, char_count)
-                left = right
+                chars[write] = char
+                write += 1
+                if char_count > 1:
+                    for digit in str(char_count):
+                        chars[write] = digit
+                        write += 1
 
-        char = chars[left]
-        char_count = n - left
-        write_compressed_char(char, char_count)
+                left = right
 
         return write
 
