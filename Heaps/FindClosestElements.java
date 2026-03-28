b8730ba73b7dc39d760f0b1b9ad121094689b4e9
Dalton Pettus <daltpettus@gmail.com>
2026-03-28 13:55:15 -0700

    clarify java solution

--- Heaps/FindClosestElements.java
+++ Heaps/FindClosestElements.java
@@ -5,9 +5,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
-public class FindKClosestElements {
-    public List<Integer> findKClosestElements(int[] arr, int k, int x) {
+public class FindClosestElements {
+    public List<Integer> findKClosestElementsHeap(int[] arr, int k, int x) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((n1, n2) -> {
+            // We want to sort the heap based on the distance of the numbers from x, popping
+            // numbers with larger distances first,
+            // with a tie-breaking rule that favors keeping smaller numbers.
             int n1Diff = Math.abs(x - n1);
             int n2Diff = Math.abs(x - n2);
             if (n1Diff == n2Diff) {
@@ -18,7 +21,8 @@ public class FindKClosestElements {
 
         for (int num : arr) {
             maxHeap.add(num);
-            if (maxHeap.size() > k) maxHeap.remove();
+            if (maxHeap.size() > k)
+                maxHeap.remove();
         }
 
         List<Integer> kClosestNums = new ArrayList<>();
@@ -29,4 +33,7 @@ public class FindKClosestElements {
         Collections.sort(kClosestNums);
         return kClosestNums;
     }
+    // Time Complexity: O(n log k) to build the heap and O(k log k) to sort the
+    // result, leading to O(n log k + k log k) overall.
+    // Space Complexity: O(k) for the heap and O(k) for the result list.
 }
