75bc5e62a5aa40afb51b1c07e09ee6ad1a4c7fb6
Dalton Pettus <daltpettus@gmail.com>
2026-04-01 11:09:05 -0700

    refactor js solution

--- Greedy/asteroidsDestroyed.js
+++ Greedy/asteroidsDestroyed.js
@@ -1,9 +1,14 @@
 const asteroidsDestroyed = (mass, asteroids) => {
-    const asteroidsSorted = asteroids.toSorted((a, b) => a - b);
-    for (let asteroid of asteroidsSorted) {
-        if (asteroid > mass) return false;
-        mass += asteroid;
-    }
+  const asteroidsSorted = asteroids.toSorted((a, b) => a - b);
+  for (const asteroid of asteroidsSorted) {
+    if (asteroid > mass) return false;
+    mass += asteroid;
+  }
 
-    return true;
-}
+  return true;
+};
+
+// Time Complexity: O(n log n) due to sorting the asteroids array, 
+// where n is the length of the asteroids array.
+// Space Complexity: O(n) due to the space used by the sorted asteroids array, 
+// could be O(1) if we sort the original array in place.
