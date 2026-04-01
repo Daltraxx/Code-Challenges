d8f765d5c666889f240003096adfab4f15ab0594
Dalton Pettus <daltpettus@gmail.com>
2026-03-31 22:08:48 -0700

    refactor java solution

--- SlidingWindow/FindMaxAverage.java
+++ SlidingWindow/FindMaxAverage.java
@@ -5,20 +5,25 @@ package SlidingWindow;
 Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
 Any answer with a calculation error less than 10-5 will be accepted. */
 
-public class MaximumAverageSubarray {
+public class FindMaxAverage {
     public double findMaxAverage(int[] nums, int k) {
-        int currentSum = 0;
+        long currentSum = 0;
         for (int i = 0; i < k; i++) {
             currentSum += nums[i];
         }
 
-        double maxSum = currentSum;
+        long maxSum = currentSum;
 
         for (int i = k; i < nums.length; i++) {
             currentSum += nums[i] - nums[i - k];
             maxSum = Math.max(currentSum, maxSum);
         }
 
-        return maxSum / k;
+        return (double) maxSum / k;
     }
 }
+
+// Time complexity: O(n) where n is the length of the input array nums, as we
+// need to iterate through the array once to calculate the sum of each subarray
+// of size k.
+// Space complexity: O(1)
