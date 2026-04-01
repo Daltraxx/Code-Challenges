f0c67474633e3c93745b7bc7eed76fe9fd4334f5
Dalton Pettus <daltpettus@gmail.com>
2026-03-31 21:18:07 -0700

    refactor

--- Heaps/kth_largest.py
+++ Heaps/kth_largest.py
@@ -1,8 +1,8 @@
-from heapq import heappop, heappush
+from heapq import heappop, heappush, heapreplace
 from typing import List
 
 
-class Kth_Largest:
+class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.scores = []
         self.k = k
@@ -16,10 +16,11 @@ class Kth_Largest:
     # Space complexity: O(k) for the heap, as we are maintaining at most k elements in the heap.
 
     def add(self, val: int) -> int:
-        heappush(self.scores, val)
-        if len(self.scores) > self.k:
-            heappop(self.scores)
+        if len(self.scores) < self.k:
+            heappush(self.scores, val)
+        elif val > self.scores[0]:
+            heapreplace(self.scores, val)
         return self.scores[0]
 
-    # Time complexity for add method: O(log k) for 
-    # inserting the new value into the heap and maintaining its size.
+    # Time complexity for add method: O(log k) in the worst case, 
+    # when we need to replace the smallest element in the heap.
