bf91b8abb42db6aae02f253bc323e1d28effd07b
Dalton Pettus <daltpettus@gmail.com>
2026-04-02 14:23:22 -0700

    partition array such that max diff is k python solution

---
+++ Greedy/partition_array.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class PartitionArray:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        count = 0
+        left = 0
+        for right in range(len(nums)):
+            if nums[right] - nums[left] > k:
+                left = right
+                count += 1
+
+        return count + 1
+
+    # Time complexity: O(n log n) due to the sorting step,
+    # where n is the length of the input array nums.
+    # The two-pointer traversal takes O(n) time,
+    # but the sorting dominates the overall time complexity.
+    # Space complexity: O(1)
