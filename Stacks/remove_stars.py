d1caf8ce1d77a397dac5bfe0fd45c2e1d60e17d6
Dalton Pettus <daltpettus@gmail.com>
2026-04-02 21:14:55 -0700

    add comment

--- Stacks/remove_stars.py
+++ Stacks/remove_stars.py
@@ -3,6 +3,9 @@ class RemoveStars:
         stack = []
         for char in s:
             if char == "*":
+                # Guaranteed by the problem constrains that 
+                # there will always be a non-star character before the star character, 
+                # so we can safely pop from the stack without checking if it's empty.
                 stack.pop()
             else:
                 stack.append(char)
