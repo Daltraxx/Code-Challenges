0a311b770cd6b6c5d7c7d3adf77e3da233fa7330
Dalton Pettus <daltpettus@gmail.com>
2026-04-07 14:40:54 -0700

    rename

--- SlidingWindow/numSubarrayProductLessThanK.js
+++ SlidingWindow/numSubarrayProductLessThanK.js
@@ -1,7 +1,7 @@
 const numSubarrayProductLessThanK = (nums, k) => {
   if (k <= 1) return 0;
 
-  let numSubArrays = 0;
+  let numSubarrays = 0;
   let currentProduct = 1;
   let left = 0;
 
@@ -12,10 +12,10 @@ const numSubarrayProductLessThanK = (nums, k) => {
       left++;
     }
 
-    numSubArrays += right - left + 1;
+    numSubarrays += right - left + 1;
   }
 
-  return numSubArrays;
+  return numSubarrays;
 };
 
 // Time Complexity: O(n) where n is the length of the input array
