083dfcc603180d937268203114016f54ea65c545
Dalton Pettus <daltpettus@gmail.com>
2026-04-06 17:17:42 -0700

    refactor

--- Greedy/find_least_num_unique_ints.py
+++ Greedy/find_least_num_unique_ints.py
@@ -5,12 +5,19 @@ from typing import List
 class FindLeastNumUniqueInts:
     def findLeastNumUniqueInts(self, arr: List[int], k: int) -> int:
         frequencies = Counter(arr)
-        sorted_frequencies = sorted(frequencies, key=frequencies.get)
-        for num in sorted_frequencies:
-            if k >= frequencies[num]:
-                k -= frequencies[num]
-                del frequencies[num]
+        frequency_vals = sorted(frequencies.values())
+        unique_ints = len(frequencies)
+        for frequency in frequency_vals:
+            if k >= frequency:
+                k -= frequency
+                unique_ints -= 1
             else:
                 break
 
-        return len(frequencies)
+        return unique_ints
+
+    # Time complexity: O(n log n) where n is the number of elements in the input array.
+    # We need to count the frequency of each integer, which takes O(n) time.
+    # Sorting the frequency values takes O(m log m) time, where m is the number of unique integers.
+    # In the worst case, if all integers are unique, m can be equal to n, leading to O(n log n) time.
+    # Space complexity: O(m) where m is the number of unique integers in the input array.
