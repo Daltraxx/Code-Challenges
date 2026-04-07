1786f1505c514758f861b1ee0d1b800fb122cb30
Dalton Pettus <daltpettus@gmail.com>
2026-04-07 14:04:59 -0700

    refactor java solution

--- Greedy/NumRescueBoats.java
+++ Greedy/NumRescueBoats.java
@@ -1,21 +1,26 @@
 import java.util.Arrays;
 
 public class NumRescueBoats {
-  public int numRescueBoats(int[] people, int limit) {
-        int boatsNeeded = 0;
-        int i = 0;
-        int j = people.length - 1;
+    public int numRescueBoats(int[] people, int limit) {
+        int boats = 0;
+        int left = 0;
+        int right = people.length - 1;
         Arrays.sort(people);
-        
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
             }
-            
-            j--;
-            boatsNeeded++;
+            // Heaviest person always goes on a boat,
+            // so we move the right pointer
+            // regardless of whether we can fit the lightest person with them or not.
+            right--;
+            boats++;
         }
-        
-        return boatsNeeded;
+
+        return boats;
     }
 }
+
+// Time complexity: O(n log n) due to sorting the array of people.
+// Space complexity: O(1) since we sort the array in place.
