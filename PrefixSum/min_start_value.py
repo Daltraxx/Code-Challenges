4592d413cec786f757f542b87daee27d91e88ba1
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 14:14:41 -0700

    renaming

--- PrefixSum/min_start_value.py
+++ PrefixSum/min_start_value.py
@@ -8,24 +8,24 @@ class MinStartValue:
         min_val = nums[0]
         for i in range(1, len(nums)):
             prefix.append(nums[i] + prefix[i - 1])
-            min_val = min(prefix[i], min_val)
+            min_prefix_sum = min(prefix[i], min_val)
 
-        if min_val >= 1:
+        if min_prefix_sum >= 1:
             return 1
 
-        return -min_val + 1
+        return -min_prefix_sum + 1
 
     # Time complexity: O(n)
     # Space complexity: O(n)
 
     def minStartValueOptimized(self, nums: List[int]) -> int:
         prefix_sum = 0
-        min_val = 0
+        min_prefix_sum = 0
         for num in nums:
             prefix_sum += num
-            min_val = min(prefix_sum, min_val)
+            min_prefix_sum = min(prefix_sum, min_prefix_sum)
 
-        return 1 - min_val
+        return 1 - min_prefix_sum
     
     # Time complexity: O(n)
     # Space complexity: O(1)
