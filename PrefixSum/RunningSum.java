0831d4dd6cdc473ba2c1ebb9b6280069d673a74c
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 13:39:41 -0700

    add package

--- PrefixSum/RunningSum.java
+++ PrefixSum/RunningSum.java
@@ -1,6 +1,4 @@
-/*Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
-
-Return the running sum of nums. */
+package PrefixSum;
 
 class RunningSum {
     public int[] runningSum(int[] nums) {
