5d875f68b426660d5c93110a4dd231de54552da2
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 14:24:00 -0700

    formatting

--- PrefixSum/minStartValue.js
+++ PrefixSum/minStartValue.js
@@ -5,8 +5,8 @@ const minStartValue = (nums) => {
   for (let num of nums) {
     prefixSum += num;
     minPrefixSum = Math.min(prefixSum, minPrefixSum);
-    }
-    
+  }
+
   // If the real minimum prefix sum is greater than 0,
   // minPrefixSum will be 0 thanks to how it is initialized,
   // so we can just return 1 - minPrefixSum in all cases.
