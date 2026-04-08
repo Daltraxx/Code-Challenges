201d7f5a60d022f764f79b4d4dd6fabbca52acc9
Dalton Pettus <daltpettus@gmail.com>
2026-04-08 14:08:49 -0700

    refactor

--- PrefixSum/min_start_value.py
+++ PrefixSum/min_start_value.py
@@ -18,17 +18,14 @@ class MinStartValue:
     # Time complexity: O(n)
     # Space complexity: O(n)
 
-    def minStartValueConstantSpace(self, nums: List[int]) -> int:
-        prefix_sum = nums[0]
-        min_val = prefix_sum
-        for i in range(1, len(nums)):
-            prefix_sum += nums[i]
+    def minStartValueOptimized(self, nums: List[int]) -> int:
+        prefix_sum = 0
+        min_val = 0
+        for num in nums:
+            prefix_sum += num
             min_val = min(prefix_sum, min_val)
 
-        if min_val >= 1:
-            return 1
-
-        return -min_val + 1
+        return 1 - min_val
     
     # Time complexity: O(n)
     # Space complexity: O(1)
