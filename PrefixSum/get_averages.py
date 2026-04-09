b94921d590da9736296872a8ec98675df2d4cc1a
Dalton Pettus <daltpettus@gmail.com>
2026-04-09 11:26:35 -0700

    sliding window solution

--- PrefixSum/get_averages.py
+++ PrefixSum/get_averages.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class GetAverages:
-    def getAverages(self, nums: List[int], k: int) -> List[int]:
+    def getAveragesPrefixSum(self, nums: List[int], k: int) -> List[int]:
         n = len(nums)
         avgs = [-1] * n
         prefix = [nums[0]]
@@ -14,5 +14,20 @@ class GetAverages:
             curr_sum = prefix[i + k] - left_sum
             avgs[i] = curr_sum // (k * 2 + 1)
             left_sum = prefix[i - k]
+
+        return avgs
+
+    def getAveragesSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        avgs = [-1] * n
+        window_size = k * 2 + 1
+        curr_sum = sum(nums[: k * 2])
+        left = 0
+        for right in range(k * 2, n):
+            curr_sum += nums[right]
+            avgs[right - k] = curr_sum // window_size
+            # Set up for next iteration
+            curr_sum -= nums[left]
+            left += 1
 
         return avgs
