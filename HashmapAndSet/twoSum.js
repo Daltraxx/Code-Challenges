e0d0f6040d4a6d4049e47a395a4d03e8812d9209
Dalton Pettus <daltpettus@gmail.com>
2026-04-09 19:21:28 -0700

    refactor js solution

--- HashmapAndSet/twoSum.js
+++ HashmapAndSet/twoSum.js
@@ -1,22 +1,20 @@
-/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.*/
-
 const twoSum = (nums, target) => {
-    const numToIndexMap = new Map();
+  const numToIndexMap = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
+      const num = nums[i];
+    const complement = target - num;
 
-        if (numToIndexMap.has(complement)) {
-            return [i, numToIndexMap.get(complement)];
-        }
-
-        numToIndexMap.set(nums[i], i);
+    if (numToIndexMap.has(complement)) {
+      return [numToIndexMap.get(complement), i];
     }
 
-    //return empty array if no solution found
-    return [];
-}
+    numToIndexMap.set(num, i);
+  }
+  return [];
+};
+
+// Time complexity: O(n) - We traverse the array once, 
+// and each lookup and insertion in the map is O(1).
+// Space complexity: O(n) - In the worst case, 
+// we could store all n elements in the map if no two numbers sum up to the target.
