459f94b17e7ab6f8efd91f8def9769ab2eec8b92
Dalton Pettus <daltpettus@gmail.com>
2026-04-09 19:22:01 -0700

    formatting

--- HashmapAndSet/twoSum.js
+++ HashmapAndSet/twoSum.js
@@ -1,8 +1,8 @@
 const twoSum = (nums, target) => {
   const numToIndexMap = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-      const num = nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
     const complement = target - num;
 
     if (numToIndexMap.has(complement)) {
@@ -14,7 +14,7 @@ const twoSum = (nums, target) => {
   return [];
 };
 
-// Time complexity: O(n) - We traverse the array once, 
+// Time complexity: O(n) - We traverse the array once,
 // and each lookup and insertion in the map is O(1).
-// Space complexity: O(n) - In the worst case, 
+// Space complexity: O(n) - In the worst case,
 // we could store all n elements in the map if no two numbers sum up to the target.
