6b85519128e07bce9f473062a0bcf0c841527f5e
Dalton Pettus <daltpettus@gmail.com>
2026-04-11 10:48:44 -0700

    refactor

--- HashmapAndSet/intersection.py
+++ HashmapAndSet/intersection.py
@@ -3,18 +3,20 @@ from typing import List
 
 
 class Intersection:
-    def intersection(self, nums: List[List[int]]) -> int:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
         n = len(nums)
         counts = defaultdict(int)
-        for i in range(n):
-            for j in range(len(nums[i])):
-                num = nums[i][j]
+        for arr in nums:
+            for num in arr:
                 counts[num] += 1
 
-        ans = []
-        for num in counts:
-            if counts[num] == n:
-                ans.append(num)
-
-        ans.sort()
-        return ans
+        return sorted([num for num, count in counts.items() if count == n])
+    
+    # Time complexity: O(n + k log k) 
+    # where n is the total number of elements across all input arrays 
+    # and k is the number of unique elements in the counts dictionary. 
+    # We iterate through all elements to populate the counts dictionary, 
+    # which takes O(n) time. 
+    # Sorting the result takes O(k log k) time.
+    # Space complexity: O(k) where k 
+    # is the number of unique elements in the counts dictionary.
