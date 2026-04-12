d9ece9b756a64f5cafe56864d43d8aa863466bdc
Dalton Pettus <daltpettus@gmail.com>
2026-04-12 14:27:35 -0700

    refactor

--- HashmapAndSet/subarray_sum.py
+++ HashmapAndSet/subarray_sum.py
@@ -9,13 +9,13 @@ class SubarraySum:
         # because a subarray that starts from the beginning of the array can sum to k.
         prefix_counts[0] = 1
         curr_sum = 0
-        subarrays = 0
+        subarray_count = 0
         for num in nums:
             curr_sum += num
-            subarrays += prefix_counts[curr_sum - k]
+            subarray_count += prefix_counts[curr_sum - k]
             prefix_counts[curr_sum] += 1
 
-        return subarrays
+        return subarray_count
 
     # Time complexity: O(n) where n is the number of elements in the input array.
     # We iterate through the input array once, 
