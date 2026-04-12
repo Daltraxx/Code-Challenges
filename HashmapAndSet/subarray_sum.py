684fa11f4910f9732fb36ee77f636570a92c18b7
Dalton Pettus <daltpettus@gmail.com>
2026-04-12 13:11:21 -0700

    subarray sum equals k

---
+++ HashmapAndSet/subarray_sum.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from typing import List
+
+
+class SubarraySum:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixes = defaultdict(int)
+        prefixes[0] = 1
+        curr_sum = 0
+        subarrays = 0
+        for num in nums:
+            curr_sum += num
+            if curr_sum - k in prefixes:
+                subarrays += prefixes[curr_sum - k]
+            prefixes[curr_sum] += 1
+
+        return subarrays
+
+    # Time complexity: O(n) where n is the number of elements in the input array.
+    # We iterate through the input array once, 
+    # performing constant time operations for each element.
+    # Space complexity: O(n) in the worst case, 
+    # where n is the number of elements in the input array.
