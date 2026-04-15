7fd0aa6e68c1cb5db7d6aeb256bdc12f813c8de7
Dalton Pettus <daltpettus@gmail.com>
2026-04-14 17:19:10 -0700

    refactor

--- HashmapAndSet/max_number_of_balloons.py
+++ HashmapAndSet/max_number_of_balloons.py
@@ -15,33 +15,30 @@ class MaxNumberOfBalloons:
                 else:
                     char_counts[char] -= 1
             if letters_remain:
-              balloon_count += 1
+                balloon_count += 1
 
         return balloon_count
-    
+
     # Time complexity: O(n) where n is the number of characters in the input string.
-    # We iterate through the input string once to count the characters, 
-    # and then we iterate through the characters in the word "balloon" 
-    # a number of times that is at most 
+    # We iterate through the input string once to count the characters,
+    # and then we iterate through the characters in the word "balloon"
+    # a number of times that is at most
     # the number of characters in the input string divided by the length of "balloon".
-    # Space complexity: O(1) since the character counts are stored 
+    # Space complexity: O(1) since the character counts are stored
     # in a fixed size hash map with at most 26 entries (assuming only lowercase letters).
-    
+
     def maxNumberOfBalloonsMath(self, text: str) -> int:
-        one_required = ["b", "a", "n"]
-        two_required = ["l", "o"]
         char_counts = Counter(text)
-        min_possible = float("inf")
-        for char in one_required:
-            min_possible = min(char_counts[char], min_possible)
-        for char in two_required:
-            can_make = char_counts[char] // 2
-            min_possible = min(can_make, min_possible)
-        return min_possible
-    
+        return min(
+            char_counts["b"],
+            char_counts["a"],
+            char_counts["l"] // 2,
+            char_counts["o"] // 2,
+            char_counts["n"],
+        )
+
     # Time complexity: O(n) where n is the number of characters in the input string.
     # We iterate through the input string once to count the characters,
     # and then we iterate through the characters in the word "balloon" a constant number of times.
-    # Space complexity: O(1) since the character counts are stored in a 
+    # Space complexity: O(1) since the character counts are stored in a
     # fixed size hash map with at most 26 entries (assuming only lowercase letters).
-
