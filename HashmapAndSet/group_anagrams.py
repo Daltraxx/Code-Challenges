d5cffa5defc3e6e37b97deca2263d48bc92f5059
Dalton Pettus <daltpettus@gmail.com>
2026-04-16 15:21:11 -0700

    group anagrams python solution

---
+++ HashmapAndSet/group_anagrams.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from typing import List
+
+
+class GroupAnagrams:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for s in strs:
+            freq = [0] * 26
+            for c in s:
+                freq[ord(c) - ord("a")] += 1
+            freq_tuple = tuple(freq)
+            groups[freq_tuple].append(s)
+
+        return list(groups.values())
+
+    # Time complexity: O(n * m) where n is the number of strings in the input array 
+    # and m is the maximum length of a string in the input array.
+    # We iterate through the input array once, and for each string, 
+    # we compute its frequency tuple in O(m) time.
+    # Space complexity: O(n * m) in the worst case where all strings are unique and have a length of m, 
+    # resulting in n unique frequency tuples stored in the hashmap.
