e9f8f3dda49b89fe8d2ed03cd3b0e6aa5a05a504
Dalton Pettus <daltpettus@gmail.com>
2026-04-20 10:09:09 -0700

    add optimized solution

--- HashmapAndSet/length_of_longest_substring.py
+++ HashmapAndSet/length_of_longest_substring.py
@@ -11,9 +11,26 @@ class LengthOfLongestSubstring:
             longest_substring = max(right - left + 1, longest_substring)
 
         return longest_substring
-    
+
     # Time complexity: O(n) where n is the length of the input string.
-    # We iterate through the input string once, 
+    # We iterate through the input string once,
     # and each character is added and removed from the set at most once.
-    # Space complexity: O(min(m, n)), or O(n) in the worst case 
+    # Space complexity: O(min(m, n)), or O(n) in the worst case
     # where all characters are unique.
+
+    def lengthOfLongestSubstringOptimized(self, s: str) -> int:
+        char_to_index = {}
+        longest_substring = 0
+        left = 0
+        for right, char in enumerate(s):
+            prev_index = char_to_index.get(char)
+            if prev_index is not None:
+                # Max necessary to ensure left pointer only moves forward, 
+                # in case of repeated characters that have already been skipped over.
+                left = max(char_to_index[char] + 1, left)
+            char_to_index[char] = right
+            longest_substring = max(right - left + 1, longest_substring)
+
+        return longest_substring
+    
+    # Same time and space complexity as the previous method, but faster in practice.
