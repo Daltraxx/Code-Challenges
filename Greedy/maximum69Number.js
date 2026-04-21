f766190b1ce8027fd63eafcad9066dd38b480dd8
Dalton Pettus <daltpettus@gmail.com>
2026-04-21 16:14:20 -0700

    js solution

--- Greedy/maximum69Number.js
+++ Greedy/maximum69Number.js
@@ -1,14 +1,14 @@
 const maximum69Number = (num) => {
-  num = String(num);
-  const firstSix = num.indexOf("6");
-
-  if (firstSix !== -1) {
-    num = num.slice(0, firstSix) + "9" + num.slice(firstSix + 1);
+  const digits = String(num).split("");
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] === "6") {
+      digits[i] = "9";
+      break;
+    }
   }
 
-  return Number(num);
+  return Number(digits.join(""));
 };
 
-// linear time and constant space
-
-console.log(maximum69Number(9969));
+// Time Complexity: O(n) where n is the number of digits in the input number
+// Space Complexity: O(n) where n is the number of digits in the input number
