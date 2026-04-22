60aa90902bb437692db60e54658af44f655ff09d
Dalton Pettus <daltpettus@gmail.com>
2026-04-22 16:21:52 -0700

    refactor

--- Greedy/min_set_size.py
+++ Greedy/min_set_size.py
@@ -6,6 +6,8 @@ class MinSetSize:
         freqs = Counter(arr)
         sortedFreqs = sorted(freqs.values(), reverse=True)
         removed = 0
+        # Integer division for calculating half is safe here since arr.length 
+        # is guaranteed to be even according to the problem statement.
         half = len(arr) // 2
         set_size = 0
         for freq in sortedFreqs:
@@ -18,3 +20,6 @@ class MinSetSize:
     # This is because we need to sort the frequencies of the elements, 
     # which takes O(n log n) time in the worst case where all elements are unique.
     # Space complexity: O(n) in the worst case, where all elements in the input array are unique.
+
+arr = [3, 2, 1]
+print(MinSetSize().minSetSize(arr))
