368802c6c8184e7f7342bd1b8c6e62b3b4bd300f
Dalton Pettus <daltpettus@gmail.com>
2026-04-22 12:51:13 -0700

    how many apples can you put into the basket python solution

---
+++ Greedy/max_number_of_apples.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class MaxNumberOfApples:
+    def maxNumberOfApples(self, weight: List[int]) -> int:
+        limit = 5000
+        max_apples = 0
+        sorted_weight = sorted(weight)
+        for weight in sorted_weight:
+            if limit >= weight:
+                limit -= weight
+                max_apples += 1
+            else:
+                break
+
+        return max_apples
+
+    # Time complexity: O(n log n) where n is the number of elements in the input list weight.
+    # This is because we need to sort the list of weights, 
+    # which takes O(n log n) time. 
+    # After sorting, we iterate through the sorted list once, 
+    # which takes O(n) time. Therefore, the overall time complexity is O(n log n).
+    # Space complexity: O(n) for the sorted list of weights. 
+    # Could sort in place to reduce space complexity to O(1).
