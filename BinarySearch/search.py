41e4d777b09d8df366b9c8648a84cce890e6e039
Dalton Pettus <daltpettus@gmail.com>
2026-04-23 10:28:17 -0700

    binary search python solution

---
+++ BinarySearch/search.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Search:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+
+    # Time complexity: O(log n) where n is the number of elements in the input array.
+    # This is because we are halving the search space in each iteration of the loop.
+    # Space complexity: O(1) because we are using a constant amount of extra space.
