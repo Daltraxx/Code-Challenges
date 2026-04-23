18832aa7bfcd43e33f472115973fe3494de928ba
Dalton Pettus <daltpettus@gmail.com>
2026-04-23 10:36:00 -0700

    refactor java solution

--- BinarySearch/Search.java
+++ BinarySearch/Search.java
@@ -4,19 +4,19 @@ public class Search {
     int right = nums.length - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (nums[mid] == target)
+      if (nums[mid] == target) {
         return mid;
-
-      if (nums[mid] > target) {
+      } else if (nums[mid] > target) {
         right = mid - 1;
       } else {
         left = mid + 1;
       }
     }
-    
+
     return -1;
   }
 }
 
-// Time O(logn)
-// Space O(1)
+// Time complexity: O(log n) where n is the number of elements in the input array.
+// This is because we are halving the search space in each iteration of the loop.
+// Space complexity: O(1) because we are using a constant amount of extra space.
