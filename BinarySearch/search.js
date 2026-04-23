77eea9d44a947d5dc8accd19459cd2f08d249354
Dalton Pettus <daltpettus@gmail.com>
2026-04-23 10:34:27 -0700

    refactor js solution

--- BinarySearch/search.js
+++ BinarySearch/search.js
@@ -1,11 +1,11 @@
 const search = (nums, target) => {
-  let left = 0,
-    right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-
-    if (nums[mid] > target) {
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
       right = mid - 1;
     } else {
       left = mid + 1;
@@ -15,5 +15,6 @@ const search = (nums, target) => {
   return -1;
 };
 
-// Time O(logn)
-// Space O(1)
+// Time complexity: O(log n) where n is the number of elements in the input array.
+// This is because we are halving the search space in each iteration of the loop.
+// Space complexity: O(1) because we are using a constant amount of extra space.
