14b8f6e4c92679e9865fdc3c1c2161f7306de976
Dalton Pettus <daltpettus@gmail.com>
2026-04-24 11:55:17 -0700

    js solution

--- BinarySearch/successfulPairs.js
+++ BinarySearch/successfulPairs.js
@@ -2,7 +2,8 @@ const successfulPairs = (spells, potions, success) => {
   potions.sort((a, b) => a - b);
   const pairs = [];
   for (let spell of spells) {
-    let left = 0, right = potions.length;
+    let left = 0
+    let right = potions.length;
     const target = success / spell;
     while (left < right) {
       const mid = Math.floor((left + right) / 2);
@@ -13,17 +14,22 @@ const successfulPairs = (spells, potions, success) => {
       }
     }
 
+    // After the binary search, left will be the index of the first potion
+    // that is greater than or equal to the target.
     pairs.push(potions.length - left);
   }
 
   return pairs;
 }
 
-// Time complexity O(mlogm+nlogm) or O((m+n)⋅logm)
-// O(n) space complexity for pairs array
-
-const spells = [5, 1, 3],
-  potions = [1, 2, 3, 4, 5],
-  success = 7;
-
-console.log(successfulPairs(spells, potions, success));
+// Time complexity: O((m + n) log m) 
+// where m is the number of elements in the potions array
+// and n is the number of elements in the spells array.
+// This is because we first sort the potions array,
+// which takes O(m log m) time,
+// and then for each spell, we perform a binary search on the sorted potions
+// array, which takes O(log m) time.
+// Since we do this for n spells,
+// the total time complexity is O((m + n) log m).
+// Space complexity: O(1) if we don't count the space used for the output array,
+// or O(n) if we do count the output array.
