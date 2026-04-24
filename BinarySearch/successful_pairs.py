00f1d5d046f5ba86f87d12e020ff4c64fa9027f4
Dalton Pettus <daltpettus@gmail.com>
2026-04-23 20:35:57 -0700

    lib function version

--- BinarySearch/successful_pairs.py
+++ BinarySearch/successful_pairs.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from typing import List
 
 
@@ -9,7 +10,9 @@ class SuccessfulPairs:
         sorted_potions = sorted(potions)
         pairs = []
         for spell in spells:
-            target = success / spell
+            # The formula (success + spell - 1) // spell
+            # is a common way to perform ceiling division for positive integers.
+            target = (success + spell - 1) // spell
             left = 0
             right = m
             while left < right:
@@ -20,15 +23,32 @@ class SuccessfulPairs:
                 else:
                     left = mid + 1
             pairs.append(m - left)
-        
+
         return pairs
 
-    # Time complexity: O((m + n) log m) where m is the number of elements in the potions array 
+    # Time complexity: O((m + n) log m) where m is the number of elements in the potions array
     # and n is the number of elements in the spells array.
-    # This is because we first sort the potions array, 
-    # which takes O(m log m) time, 
-    # and then for each spell, we perform a binary search on the sorted potions array, 
-    # which takes O(log m) time. 
-    # Since we do this for n spells, 
+    # This is because we first sort the potions array,
+    # which takes O(m log m) time,
+    # and then for each spell, we perform a binary search on the sorted potions array,
+    # which takes O(log m) time.
+    # Since we do this for n spells,
     # the total time complexity is O((m + n) log m).
     # Space complexity: O(m) due to the space used to store the sorted potions array.
+
+    def successfulPairsBisect(
+        self, spells: List[int], potions: List[int], success: int
+    ) -> List[int]:
+        m = len(potions)
+        sorted_potions = sorted(potions)
+        pairs = []
+        for spell in spells:
+            target = (success + spell - 1) // spell
+            idx = bisect_left(sorted_potions, target)
+            pairs.append(m - idx)
+
+        return pairs
+    
+    # Same time and space complexity as the previous method, 
+    # but uses the built-in bisect_left function for binary search, 
+    # which can be more concise and potentially optimized.
