bff6c895195ce6c569658a6aa0644b13ce163619
Dalton Pettus <daltpettus@gmail.com>
2026-04-24 17:44:57 -0700

    js solution

--- BinarySearch/searchInsert.js
+++ BinarySearch/searchInsert.js
@@ -1,9 +1,11 @@
 const searchInsert = (nums, target) => {
-  let left = 0; right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) {
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
       left = mid + 1;
     } else {
       right = mid - 1;
@@ -11,12 +13,7 @@ const searchInsert = (nums, target) => {
   }
 
   return left;
-}
+};
 
-// Time complexity O(logn)
-// Space complexity O(1)
-
-const nums = [1, 3, 5, 6],
-  target = 2;
-
-console.log(searchInsert(nums, target));
+// Time complexity: O(log n) where n is the length of the input array.
+// Space complexity: O(1) since we are using only a constant amount of extra space.
