2cf1efe75ca1900c8d92362bbd37d077a3fbd269
Dalton Pettus <daltpettus@gmail.com>
2026-04-26 09:48:22 -0700

    refactor

--- LinkedLists/reverse_list.py
+++ LinkedLists/reverse_list.py
@@ -5,14 +5,12 @@ from list_node import ListNode
 
 class ReverseList:
     def reverseList(self, head: Optional[ListNode]) -> ListNode:
-        if not head:
-            return head
         prev = None
         while head:
-            next = head.next
+            next_node = head.next
             head.next = prev
             prev = head
-            head = next
+            head = next_node
 
         return prev
     
@@ -21,10 +19,24 @@ class ReverseList:
             if not current:
                 return prev
             
-            next = current.next
+            next_node = current.next
             current.next = prev
-            return reverse_list(current, next)
+            return reverse_list(current, next_node)
         
         return reverse_list(None, head)
+    
+    def reverseListRecursiveCanonical(self, head: Optional[ListNode]) -> ListNode:
+        if not head or not head.next:
+            return head
         
-
+        new_head = self.reverseListRecursiveCanonical(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+        
+    # Time complexity: O(n) where n is the number of nodes in the linked list.
+    # We visit each node exactly once to reverse the pointers.
+    # Space complexity: O(1) for the iterative approach, 
+    # as we only use a constant amount of extra space.
+    # For the recursive approaches, 
+    # the space complexity is O(n) due to the call stack.
