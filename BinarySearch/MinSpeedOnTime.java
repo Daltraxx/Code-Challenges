799ea8d1d19ba850487aeb5e8691c6ee82f1e5f4
Dalton Pettus <daltpettus@gmail.com>
2026-04-29 14:51:37 -0700

    refactor java solution

--- BinarySearch/MinSpeedOnTime.java
+++ BinarySearch/MinSpeedOnTime.java
@@ -1,17 +1,22 @@
 public class MinSpeedOnTime {
-  double timeLimit;
+  int n;
+  double hour;
+  int[] dist;
+
   public int minSpeedOnTime(int[] dist, double hour) {
-    if (dist.length > Math.ceil(hour))
+    n = dist.length;
+    if (n > Math.ceil(hour)) {
       return -1;
+    }
+    this.hour = hour;
+    this.dist = dist;
 
-    timeLimit = hour;
-
-    int left = 0;
-    int right = (int) Math.pow(10, 7);
+    int left = 1;
+    int right = 10_000_000;
 
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (check(mid, dist)) {
+      if (check(mid)) {
         right = mid - 1;
       } else {
         left = mid + 1;
@@ -21,20 +26,28 @@ public class MinSpeedOnTime {
     return left;
   }
 
-  public boolean check(int speed, int[] dist) {
+  public boolean check(int speed) {
     double timeNeeded = 0;
-    for (int i = 0; i < dist.length; i++) {
-      if (i == dist.length - 1) {
-        timeNeeded += (double) dist[i] / speed;
+    for (int i = 0; i < n; i++) {
+      int d = dist[i];
+      if (i == n - 1) {
+        // Exact division for the last train
+        timeNeeded += (double) d / speed;
       } else {
-        timeNeeded += Math.ceil((double) dist[i] / speed);
+        // Ceiling division for intermediate trains
+        timeNeeded += (d + speed - 1) / speed;
+      }
+      if (timeNeeded > hour) {
+        return false;
       }
     }
 
-    return timeNeeded <= timeLimit;
+    return true;
   }
 }
 
-
-// Time O(nlogk)
-// Space O(1)
+// Time complexity: O(n * log(m)) where n is the number of distances and m is
+// the maximum speed (10^7 in this case). The log(m) factor comes from the
+// binary search on the speed values, and the O(n) factor comes from the time
+// calculation for each speed value.
+// Space complexity: O(1)
