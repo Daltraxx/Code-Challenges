12c2a13848bf2a14569ff991d741faabe6fc0fed
Dalton Pettus <daltpettus@gmail.com>
2026-04-30 10:51:59 -0700

    refactor java solution

--- BinarySearch/SmallestDivisor.java
+++ BinarySearch/SmallestDivisor.java
@@ -1,5 +1,10 @@
 public class SmallestDivisor {
+  int[] nums;
+  int threshold;
+
   public int smallestDivisor(int[] nums, int threshold) {
+    this.nums = nums;
+    this.threshold = threshold;
     int left = 1;
     int right = 0;
     for (int num : nums)
@@ -7,7 +12,7 @@ public class SmallestDivisor {
 
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (check(mid, nums, threshold)) {
+      if (check(mid)) {
         right = mid - 1;
       } else {
         left = mid + 1;
@@ -17,15 +22,22 @@ public class SmallestDivisor {
     return left;
   }
 
-  public boolean check(double divisor, int[] nums, int threshold) {
+  public boolean check(int divisor) {
     int sum = 0;
-    for (double num : nums) {
-      sum += Math.ceil(num / divisor);
+    for (int num : nums) {
+      sum += (num + divisor - 1) / divisor;
+      if (sum > threshold) {
+        return false;
+      }
     }
 
-    return sum <= threshold;
+    return true;
   }
 }
 
-// Time O(nlogk)
-// Space O(1)
+// Time complexity: O(n log m) where n is the length of nums
+// and m is the maximum value in nums.
+// The log(m) factor comes from the binary search on the divisor values,
+// and the O(n) factor comes from the check function
+// that iterates through nums for each divisor value.
+// Space complexity: O(1)
