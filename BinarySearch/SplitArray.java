b1fc3828ff5265c36ae40d0cb4d4909427547a33
Dalton Pettus <daltpettus@gmail.com>
2026-05-02 10:52:13 -0700

    refactor java solution

--- BinarySearch/SplitArray.java
+++ BinarySearch/SplitArray.java
@@ -1,29 +1,27 @@
 public class SplitArray {
   public int splitArray(int[] nums, int k) {
-    int left = Integer.MIN_VALUE;
+    int left = 0;
     int right = 0;
     for (int num : nums) {
       left = Math.max(num, left);
       right += num;
     }
 
-    int minLargestSum = 0;
     while (left <= right) {
       int mid = left + (right - left) / 2;
       if (check(mid, nums, k)) {
         right = mid - 1;
-        minLargestSum = mid;
       } else {
         left = mid + 1;
       }
     }
 
-    return minLargestSum;
+    return left;
   }
 
   private boolean check(int maxSum, int[] nums, int k) {
     int currentSum = 0;
-    int subArrayCount = 0;
+    int subArrayCount = 1;
     for (int num : nums) {
       if (currentSum + num > maxSum) {
         currentSum = 0;
@@ -34,9 +32,11 @@ public class SplitArray {
       currentSum += num;
     }
 
-    return subArrayCount + 1 <= k; // could omit this variable and just return left if desired
+    return true;
   }
 }
 
-// Time complexity O(nlogk)
-// Space O(1)
+// Time complexity: O(n log m) 
+// where n is the length of the nums array
+// and m is the sum of the nums array.
+// Space complexity: O(1).
