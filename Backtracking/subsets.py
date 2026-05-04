4f0440c5d8ff181554e79e74922f64d164d92fa4
Dalton Pettus <daltpettus@gmail.com>
2026-05-03 18:00:45 -0700

    subsets python solution

---
+++ Backtracking/subsets.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Subsets:
+    def subsets(self, nums: List[int]) -> List[int]:
+        def backtrack(curr_subset: List[int], idx: int):
+            # This condition is never true because the below loop 
+            # will never call backtrack with idx > len(nums),
+            # but is included here for clarity.
+            if idx > len(nums):
+                return
+            
+            all_subsets.append(curr_subset[:])
+            for i in range(idx, len(nums)):
+                curr_subset.append(nums[i])
+                backtrack(curr_subset, i + 1)
+                curr_subset.pop()
+
+        all_subsets = []
+        backtrack([], 0)
+        return all_subsets
+
+# Time complexity: O(n * 2^n) because there are 2^n subsets 
+# (each element can be either included or excluded) 
+# and we take O(n) time to create a copy of each subset.
+# Space complexity: O(n) for curr_subset and the recursion stack,
+# not counting the space used for the output list all_subsets, 
+# which can grow to O(n * 2^n) in the worst case.
