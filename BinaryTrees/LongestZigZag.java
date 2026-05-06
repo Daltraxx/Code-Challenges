e024f88e446ce033049c29465cc04da2ee7d2248
Dalton Pettus <daltpettus@gmail.com>
2026-05-06 10:06:02 -0700

    longest zigzag java solution

---
+++ BinaryTrees/LongestZigZag.java
@@ -0,0 +1,26 @@
+package BinaryTrees;
+
+public class LongestZigZag {
+  int maxLength;
+
+  public int longestZigZag(TreeNode root) {
+    maxLength = 0;
+    dfs(root, 0, 0);
+    return maxLength;
+  }
+
+  private void dfs(TreeNode node, int left, int right) {
+    if (node == null) {
+      return;
+    }
+
+    int currMax = Math.max(left, right);
+    maxLength = Math.max(currMax, maxLength);
+    dfs(node.left, right + 1, 0);
+    dfs(node.right, 0, left + 1);
+  }
+}
+
+// TIme Complexity: O(n) where n is the number of nodes in the tree
+// Space Complexity: O(h) where h is the height of the tree, due to the
+// recursive call stack. In the worst case of a skewed tree, this can be O(n).
