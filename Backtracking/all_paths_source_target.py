793e1af7dbfe087e780ad44c0df1ac29a6375f7c
Dalton Pettus <daltpettus@gmail.com>
2026-05-06 16:06:16 -0700

    iterative solution for funsies

--- Backtracking/all_paths_source_target.py
+++ Backtracking/all_paths_source_target.py
@@ -23,3 +23,21 @@ class AllPathsSourceTarget:
     # and each path can take O(n) time to construct.
     # Space complexity: O(n) for the recursion stack and the path storage, 
     # not including the output, which can be O(2^n * n).
+
+    def allPathsSourceTargetStack(self, graph: List[List[int]]) -> List[List[int]]:
+        stack = [(0, [0])]
+        target = len(graph) - 1
+        all_paths = []
+        while stack:
+            node, path = stack.pop()
+            for neighbor in graph[node]:
+                updated_path = path + [neighbor]
+                if neighbor == target:
+                    all_paths.append(updated_path)
+                else:
+                    stack.append((neighbor, updated_path))
+        return all_paths
+    
+    # Worse space complexity than the recursive approach due to storing all paths in the stack,
+    # and we have to copy the path for each neighbor, which costs O(n) at every step.
+    # The recursive approach is generally preferred.
