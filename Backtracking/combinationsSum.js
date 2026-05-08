e274c80dcb4cce23161d0138c987a9b81eb30255
Dalton Pettus <daltpettus@gmail.com>
2026-05-08 11:03:05 -0700

    refactor js solution

--- Backtracking/combinationsSum.js
+++ Backtracking/combinationsSum.js
@@ -1,26 +1,30 @@
 const combinationSum = (candidates, target) => {
-  let combinations = [];
-  const backtrack = (currCombo, currSum, i) => {
+  const backtrack = (currCombo, currSum, start) => {
     if (currSum === target) {
       combinations.push([...currCombo]);
       return;
     }
 
-    for (i; i < candidates.length; i++) {
+    for (let i = start; i < candidates.length; i++) {
       const num = candidates[i];
-      if (currSum + num <= target) {
-        currCombo.push(num);
-        backtrack(currCombo, currSum + num, i);
-        currCombo.pop();
+      if (currSum + num > target) {
+        break;
       }
+      currCombo.push(num);
+      backtrack(currCombo, currSum + num, i);
+      currCombo.pop();
     }
   }
 
+  const combinations = [];
+  candidates = candidates.toSorted((a, b) => a - b);
   backtrack([], 0, 0);
   return combinations;
 }
 
-const candidates = [2, 3, 6, 7],
-  target = 7;
-
-console.log(combinationSum(candidates, target));
+// TIme Complexity: O(N^T/M) where N is the number of candidates,
+// T is the target
+// and M is the average length of a valid combination.
+// Sorting the candidates takes O(N log N) time but is dominated.
+// Space Complexity: O(T/M) where T is the target 
+// and M is the average length of a valid combination
