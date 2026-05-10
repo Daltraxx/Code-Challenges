c961fc4185e4aea4f9061c080a40764b9a3fae1a
Dalton Pettus <daltpettus@gmail.com>
2026-05-10 12:23:56 -0700

    refactor

--- Backtracking/total_n_queens.py
+++ Backtracking/total_n_queens.py
@@ -3,23 +3,23 @@ from typing import List
 
 class TotalNQueens:
     def totalNQueens(self, n: int) -> int:
-        def backtrack(row: int, unsafe_cols: set[int], unsafe_left: set[int], unsafe_right: set[int]):
+        def backtrack(row: int, unsafe_cols: set[int], unsafe_anti_diag: set[int], unsafe_diag: set[int]):
             if row == n:
                 return 1
 
             solutions = 0
             for col in range(n):
                 if col not in unsafe_cols:
-                    left_diag_id = row + col
-                    right_diag_id = row - col
-                    if left_diag_id not in unsafe_left and right_diag_id not in unsafe_right:
+                    anti_diag_id = row + col
+                    diag_id = row - col
+                    if anti_diag_id not in unsafe_anti_diag and diag_id not in unsafe_diag:
                         unsafe_cols.add(col)
-                        unsafe_left.add(left_diag_id)
-                        unsafe_right.add(right_diag_id)
-                        solutions += backtrack(row + 1, unsafe_cols, unsafe_left, unsafe_right)
+                        unsafe_anti_diag.add(anti_diag_id)
+                        unsafe_diag.add(diag_id)
+                        solutions += backtrack(row + 1, unsafe_cols, unsafe_anti_diag, unsafe_diag)
                         unsafe_cols.remove(col)
-                        unsafe_left.remove(left_diag_id)
-                        unsafe_right.remove(right_diag_id)
+                        unsafe_anti_diag.remove(anti_diag_id)
+                        unsafe_diag.remove(diag_id)
 
             return solutions
 
