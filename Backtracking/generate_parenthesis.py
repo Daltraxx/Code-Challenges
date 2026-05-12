16eed367f2bc19f6d27a7ba34db41d9c0675aa34
Dalton Pettus <daltpettus@gmail.com>
2026-05-12 10:27:33 -0700

    generate parentheses python solution

---
+++ Backtracking/generate_parenthesis.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class GenerateParenthesis:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(curr: List[str], left_count: int, right_count: int):
+            if left_count == n and right_count == n - 1:
+                curr.append(")")
+                combinations.append("".join(curr))
+                curr.pop()
+                return
+
+            if left_count < n:
+                curr.append("(")
+                backtrack(curr, left_count + 1, right_count)
+                curr.pop()
+            if right_count < left_count:
+                curr.append(")")
+                backtrack(curr, left_count, right_count + 1)
+                curr.pop()
+
+        combinations = []
+        backtrack(["("], 1, 0)
+        return combinations
+
+    # Time complexity: O(2^n) because we are generating all possible combinations of parentheses, 
+    # which can be represented as a binary tree with 2^n nodes.
+    # Space complexity: O(n) because at most we will have n opening parentheses in the current combination, 
+    # which takes O(n) space.
