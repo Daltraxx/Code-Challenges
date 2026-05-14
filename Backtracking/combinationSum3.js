fa9c504156378d7a69af0bdd0e2debd576fd7692
Dalton Pettus <daltpettus@gmail.com>
2026-05-14 10:12:58 -0700

    refactor js solution

--- Backtracking/combinationSum3.js
+++ Backtracking/combinationSum3.js
@@ -1,18 +1,15 @@
 const combinationSum3 = (k, n) => {
-  const combinations = [];
-  const largestPossibleNum = n < 10 ? n : 9;
-
-  const backtrack = (currCombo, currSum, i) => {
+  const backtrack = (currCombo, currSum, startNum) => {
     if (currCombo.length === k) {
       if (currSum === n) combinations.push([...currCombo]);
       return;
     }
 
-    for (i; i <= largestPossibleNum; i++) {
-      const newSum = currSum + i;
+    for (let num = startNum; num <= largestPossibleNum; num++) {
+      const newSum = currSum + num;
       if (newSum <= n) {
-        currCombo.push(i);
-        backtrack(currCombo, newSum, i + 1);
+        currCombo.push(num);
+        backtrack(currCombo, newSum, num + 1);
         currCombo.pop();
       } else {
         return;
@@ -20,14 +17,13 @@ const combinationSum3 = (k, n) => {
     }
   };
 
+  const largestPossibleNum = Math.min(9, n - k + 1);
+  const combinations = [];
   backtrack([], 0, 1);
   return combinations;
 };
 
-// Time O(K×C(9,K))
-// Space O(K)
-
-const k = 1,
-  n = 100;
-
-console.log(combinationSum3(k, n));
+// Time complexity: O(k * C(9, k)) where C(9, k)
+// is the number of combinations of 9 numbers taken k at a time.
+// Space complexity: O(k) for the recursion stack
+// and O(C(9, k)) for storing the combinations.
