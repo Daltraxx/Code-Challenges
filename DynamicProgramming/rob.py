6ff8b60708edc50497e4e62f264d4ae8dccc2bf1
Dalton Pettus <daltpettus@gmail.com>
2026-05-15 16:27:11 -0700

    constant space solution

--- DynamicProgramming/rob.py
+++ DynamicProgramming/rob.py
@@ -20,15 +20,15 @@ class Rob:
         n = len(nums)
         dp = [None] * n
         return get_most_money(n - 1)
-    
+
     # Time complexity: O(n) where n is the number of houses.
     # Space complexity: O(n) for the recursion stack and the dp array.
-    
+
     def robBottomUp(self, nums: List[int]) -> int:
         n = len(nums)
         if n == 1:
             return nums[0]
-        
+
         dp = [-1] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
@@ -36,8 +36,26 @@ class Rob:
         for i in range(2, n):
             money = nums[i]
             dp[i] = max(money + dp[i - 2], dp[i - 1])
-        
+
         return dp[n - 1]
 
     # Time complexity: O(n) where n is the number of houses.
     # Space complexity: O(n) for the dp array.
+
+    def robConstantSpace(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+        
+        two_back = nums[0]
+        one_back = max(two_back, nums[1])
+        max_money = 0
+        for i in range(2, n):
+            money = nums[i]
+            max_money = max(money + two_back, one_back)
+            two_back = one_back
+            one_back = max_money
+
+        return max_money
