b27710ee908e7dae3336db40f06cfc66b47009d0
Dalton Pettus <daltpettus@gmail.com>
2026-05-16 14:30:27 -0700

    refactor js solution

--- DynamicProgramming/minCostClimbingStairs.js
+++ DynamicProgramming/minCostClimbingStairs.js
@@ -1,54 +1,53 @@
 const minCostClimbingStairsTopDown = (cost) => {
-  const memo = new Map();
   const dp = (i) => {
     if (i <= 1) {
       return 0;
     }
 
-    if (memo.has(i)) return memo.get(i);
-
-    memo.set(i, Math.min(dp(i - 1) + cost[i - 1], dp(i - 2) + cost[i - 2]));
-    return memo.get(i);
+    if (memo[i] !== undefined) return memo[i];
+    memo[i] = Math.min(dp(i - 2) + cost[i - 2], dp(i - 1) + cost[i - 1]);
+    return memo[i];
   };
 
+  const n = cost.length;
+  const memo = new Array(n + 1);
   return dp(cost.length);
 };
 
-// Time Complexity: O(n)
-// Space Complexity: O(n)
-
-const cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
-console.log(minCostClimbingStairsTopDown(cost));
+// Time complexity: O(n) where n is the number of steps.
+// Space complexity: O(n) for the recursion stack and the memo array.
 
 const minCostClimbingStairsBottomUp = (cost) => {
-  const dp = new Array(cost.length + 1);
+  const n = cost.length;
+  const dp = new Array(n + 1);
   dp[0] = 0;
   dp[1] = 0;
 
-  for (let i = 2; i < dp.length; i++) {
+  for (let i = 2; i < n + 1; i++) {
     dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
   }
 
   return dp.at(-1);
 };
 
-// Time Complexity: O(n)
-// Space Complexity: O(n)
+// Time complexity: O(n) where n is the number of steps.
+// Space complexity: O(n) for the dp array.
 
 const minCostClimbingStairsBottomUpConstantSpace = (cost) => {
   let downOne = 0;
   let downTwo = 0;
+  let currentCost = 0;
 
   for (let i = 2; i < cost.length + 1; i++) {
-    const temp = downOne;
-    downOne = Math.min(downOne + cost[i - 1], downTwo + cost[i - 2]);
-    downTwo = temp;
+    currentCost = Math.min(downOne + cost[i - 1], downTwo + cost[i - 2]);
+    downTwo = downOne;
+    downOne = currentCost;
   }
 
-  return downOne;
+  return currentCost;
 };
 
-// Time Complexity: O(n)
-// Space Complexity: O(1)
+// Time complexity: O(n) where n is the number of steps.
+// Space complexity: O(1) for the constant space used.
 
 console.log(minCostClimbingStairsBottomUp(cost));
