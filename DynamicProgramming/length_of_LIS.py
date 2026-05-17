969a847ac720ae7d1bebe9ff5905ed5893b932c3
Dalton Pettus <daltpettus@gmail.com>
2026-05-17 10:36:08 -0700

    bottom up solution

--- DynamicProgramming/length_of_LIS.py
+++ DynamicProgramming/length_of_LIS.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class LengthOfLIS:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLISTopDown(self, nums: List[int]) -> int:
         def get_LIS(i: int) -> int:
             if memo[i]:
                 return memo[i]
@@ -18,3 +18,20 @@ class LengthOfLIS:
         memo = [0] * n
         return max(get_LIS(i) for i in range(n))
     
+    # Time complexity: O(n^2) due to the nested calls in get_LIS.
+    # Space complexity: O(n) for the memoization array and the call stack in the
+    # worst case of a strictly increasing array.
+
+    def lengthOfLISBottomUp(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        max_subsequence = 1
+        for i in range(1, n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+                    max_subsequence = max(dp[i], max_subsequence)
+        return max_subsequence
+    
+    # Time complexity: O(n^2) due to the nested loops.
+    # Space complexity: O(n) for the dp array.
