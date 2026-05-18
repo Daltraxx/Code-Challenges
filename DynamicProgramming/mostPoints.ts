1f3e7f7fe53e23b4386dc9a6596438995e7aedfd
Dalton Pettus <daltpettus@gmail.com>
2026-05-18 11:43:36 -0700

    refactor

--- DynamicProgramming/mostPoints.ts
+++ DynamicProgramming/mostPoints.ts
@@ -1,11 +1,11 @@
 const mostPointsTopDown = (questions: number[][]): number => {
   const getMostPoints = (i: number): number => {
+    if (i >= n) return 0;
     if (memo[i] !== -1) return memo[i];
     const [points, brainPower] = questions[i];
     const nextSolvable = i + brainPower + 1;
-    const solve =
-      nextSolvable < n ? points + getMostPoints(nextSolvable) : points;
-    const skip = i + 1 < n ? getMostPoints(i + 1) : 0;
+    const solve = points + getMostPoints(nextSolvable);
+    const skip = getMostPoints(i + 1);
     memo[i] = Math.max(solve, skip);
     return memo[i];
   };
@@ -20,12 +20,12 @@ const mostPointsTopDown = (questions: number[][]): number => {
 
 const mostPointsBottomUp = (questions: number[][]): number => {
   const n = questions.length;
-  const dp = new Array(n).fill(-1);
+  const dp = new Array(n + 1).fill(0);
   dp[n - 1] = questions[n - 1][0];
-  for (let i = n - 2; i >= 0; i--) {
+  for (let i = n - 1; i >= 0; i--) {
     const [points, brainPower] = questions[i];
     const nextSolvable = i + brainPower + 1;
-    const solve = nextSolvable < n ? points + dp[nextSolvable] : points;
+    const solve = points + dp[Math.min(nextSolvable, n)];
     const skip = dp[i + 1];
     dp[i] = Math.max(solve, skip);
   }
