615f51a8029770b9a400b6e006415726f2b5c2d1
Dalton Pettus <daltpettus@gmail.com>
2026-05-18 17:00:50 -0700

    refactor

--- Heaps/max_score.py
+++ Heaps/max_score.py
@@ -4,23 +4,25 @@ from typing import List
 
 class MaxScore:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        nums = [[num1, num2] for num1, num2 in zip(nums1, nums2)]
+        nums = list(zip(nums1, nums2))
+        # Sort by num2 in descending order to ensure 
+        # we always consider the largest num2 (minimum multiplier) first.
         nums.sort(key=lambda x: x[1], reverse=True)
         min_heap = []
-        curr_min = 0
         curr_sum = 0
         max_score = 0
         for num1, num2 in nums:
             heappush(min_heap, num1)
             curr_sum += num1
-            curr_min = num2
             if len(min_heap) == k:
-                max_score = max(curr_sum * curr_min, max_score)
+                # Update max_score with the current sum of the k largest num1 values
+                # multiplied by the current min (num2).
+                max_score = max(curr_sum * num2, max_score)
                 curr_sum -= heappop(min_heap)
 
         return max_score
-    
-    # Time complexity: O(n log n) for sorting 
+
+    # Time complexity: O(n log n) for sorting
     # and O(n log k) for iterating through the sorted list and maintaining the heap.
-    # Space complexity: O(n) for the sorted list 
+    # Space complexity: O(n) for the sorted list
     # and O(k) for the heap.
