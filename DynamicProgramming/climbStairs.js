8d9e14c7f97d89f8975fdd8fb6bdd2885476a3ce
Dalton Pettus <daltpettus@gmail.com>
2026-05-20 10:01:02 -0700

    refactor js solution

--- DynamicProgramming/climbStairs.js
+++ DynamicProgramming/climbStairs.js
@@ -1,79 +1,47 @@
-const climbStairsBacktracking = (n) => {
-  let ways = 0;
-  const backtrack = (step) => {
-    if (step === n) {
-      ways++;
-      return;
-    } else if (step > n) {
-      return;
-    }
-
-    backtrack(step + 1);
-    backtrack(step + 2);
-  };
-
-  backtrack(0);
-  return ways;
-};
-
-// Time O(2^n)
-// Space O(n)
-
-const n = 4;
-console.log(climbStairsBacktracking(n));
-
 // TOP-DOWN
-// Recurrence relation: dp(i) = dp(i - 1) + dp(i - 2)
-const climbStairs = (n) => {
-  const memo = new Array(n + 1);
+const climbStairsTopDown = (n) => {
+  const memo = new Array(n + 1).fill(-1);
 
-  const getWaysToReachStep = (i) => {
+  const dp = (i) => {
     if (i === 1) return 1;
     if (i === 2) return 2;
 
-    if (memo[i] !== undefined) return memo[i];
-    memo[i] = getWaysToReachStep(i - 1) + getWaysToReachStep(i - 2);
+    if (memo[i] !== -1) return memo[i];
+    memo[i] = dp(i - 1) + dp(i - 2);
     return memo[i];
   };
 
-  return getWaysToReachStep(n);
+  return dp(n);
 };
 
-console.log(climbStairs(n));
-
-// Time O(n)
-// Space O(n)
+// Time complexity: O(n) because each subproblem is solved only once due to memoization.
+// Space complexity: O(n) for the memoization array and the call stack.
 
 // BOTTOM-UP
 const climbStairsBottomUp = (n) => {
   const dp = new Array(n + 1);
-  dp[1] = 1; // one way to get to step 1
-  dp[2] = 2 // two ways to get to step 2
+  dp[1] = 1;
+  dp[2] = 2;
   for (let i = 3; i < dp.length; i++) {
     dp[i] = dp[i - 2] + dp[i - 1];
   }
   return dp[n];
-}
+};
 
-console.log(climbStairsBottomUp(n));
-
-// Time O(n)
-// Space O(n)
+// Time complexity: O(n) due to the single loop filling the dp array.
+// Space complexity: O(n) for the dp array.
 
 const climbStairsConstantSpace = (n) => {
   let downTwo = 1;
-  let downOne = 2; // from perspective of being on third step
-  let currentStep = n <= 2 ? n : undefined;
+  let downOne = 2;
   for (let i = 3; i <= n; i++) {
-    currentStep = downOne + downTwo;
+    const currentStep = downOne + downTwo;
     downTwo = downOne;
     downOne = currentStep;
   }
 
-  return currentStep;
-}
+  return downOne;
+};
 
-console.log(climbStairsConstantSpace(n));
-
-// Time O(n)
-// Space O(1)
+// Time complexity: O(n) due to the single loop.
+// Space complexity: O(1) since we only use a constant amount of space.
